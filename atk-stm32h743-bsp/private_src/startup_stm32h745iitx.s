4cc4eb7d107a43318c1ee60313f55917834243b6
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-05-29 00:45:36 +0800

    删除  .weak SVC_Handler  .thumb_set SVC_Handler,Default_Handler
     .weak PendSV_Handler
     .thumb_set PendSV_Handler,Default_Handler
    
     .weak SysTick_Handler
     .thumb_set SysTick_Handler,Default_Handler

--- atk-stm32h743-bsp/private_src/startup_stm32h745iitx.s
+++ atk-stm32h743-bsp/private_src/startup_stm32h745iitx.s
@@ -302,15 +302,6 @@ g_pfnVectors:
 *
 *******************************************************************************/
 
-	.weak	SVC_Handler
-	.thumb_set SVC_Handler,Default_Handler
-
-	.weak	PendSV_Handler
-	.thumb_set PendSV_Handler,Default_Handler
-
-	.weak	SysTick_Handler
-	.thumb_set SysTick_Handler,Default_Handler
-
 	.weak	WWDG1_IRQHandler
 	.thumb_set WWDG1_IRQHandler,Default_Handler
 
