f9b4e851710eb21f56ff5b2573236f4f93a3668d
hjc2000 <hjc.666@foxmail.com>
2024-05-29 15:55:53 +0800

    Serialize

--- atk-stm32h743-bsp/private_src/clock/ClockSignalConfig.cpp
+++ atk-stm32h743-bsp/private_src/clock/ClockSignalConfig.cpp
@@ -3,12 +3,88 @@
 
 using namespace bsp;
 
-bsp::ClockSignalConfig::ClockSignalConfig()
+void bsp::Apb1ClkConfig::Serialize(RCC_ClkInitTypeDef &o) const
 {
-	_clock_type = ClockType::SYSCLK
-		| ClockType::HCLK
-		| ClockType::PCLK1
-		| ClockType::PCLK2;
+	o.APB1CLKDivider = static_cast<uint32_t>(_input_divider);
+}
+
+void bsp::Apb1ClkConfig::Deserialize(RCC_ClkInitTypeDef const &o)
+{
+	_input_divider = static_cast<decltype(_input_divider)>(o.APB1CLKDivider);
+}
+
+void bsp::Apb2ClkConfig::Serialize(RCC_ClkInitTypeDef &o) const
+{
+	o.APB2CLKDivider = static_cast<uint32_t>(_input_divider);
+}
+
+void bsp::Apb2ClkConfig::Deserialize(RCC_ClkInitTypeDef const &o)
+{
+	_input_divider = static_cast<decltype(_input_divider)>(o.APB2CLKDivider);
+}
+
+void bsp::Apb3ClkConfig::Serialize(RCC_ClkInitTypeDef &o) const
+{
+	o.APB3CLKDivider = static_cast<uint32_t>(_input_divider);
+}
+
+void bsp::Apb3ClkConfig::Deserialize(RCC_ClkInitTypeDef const &o)
+{
+	_input_divider = static_cast<decltype(_input_divider)>(o.APB3CLKDivider);
+}
+
+void bsp::Apb4ClkConfig::Serialize(RCC_ClkInitTypeDef &o) const
+{
+	o.APB4CLKDivider = static_cast<uint32_t>(_input_divider);
+}
+
+void bsp::Apb4ClkConfig::Deserialize(RCC_ClkInitTypeDef const &o)
+{
+	_input_divider = static_cast<decltype(_input_divider)>(o.APB4CLKDivider);
+}
+
+void bsp::HclkConfig::Serialize(RCC_ClkInitTypeDef &o) const
+{
+	o.AHBCLKDivider = static_cast<uint32_t>(_input_divider);
+	_apb1clk_config.Serialize(o);
+	_apb2clk_config.Serialize(o);
+	_apb3clk_config.Serialize(o);
+	_apb4clk_config.Serialize(o);
+}
+
+void bsp::HclkConfig::Deserialize(RCC_ClkInitTypeDef const &o)
+{
+	_input_divider = static_cast<decltype(_input_divider)>(o.AHBCLKDivider);
+	_apb1clk_config.Deserialize(o);
+	_apb2clk_config.Deserialize(o);
+	_apb3clk_config.Deserialize(o);
+	_apb4clk_config.Deserialize(o);
+}
+
+void bsp::SystemClockConfig::Serialize(RCC_ClkInitTypeDef &o) const
+{
+	o.SYSCLKSource = static_cast<uint32_t>(_clock_source);
+	o.SYSCLKDivider = static_cast<uint32_t>(_output_divider);
+	_hclk_config.Serialize(o);
+}
+
+void bsp::SystemClockConfig::Deserialize(RCC_ClkInitTypeDef const &o)
+{
+	_clock_source = static_cast<decltype(_clock_source)>(o.SYSCLKSource);
+	_output_divider = static_cast<decltype(_output_divider)>(o.SYSCLKDivider);
+	_hclk_config.Deserialize(o);
+}
+
+void bsp::ClockSignalConfig::Serialize(RCC_ClkInitTypeDef &o) const
+{
+	o.ClockType = static_cast<uint32_t>(_clock_type);
+	_system_clk_config.Serialize(o);
+}
+
+void bsp::ClockSignalConfig::Deserialize(RCC_ClkInitTypeDef const &o)
+{
+	_clock_type = static_cast<decltype(_clock_type)>(o.ClockType);
+	_system_clk_config.Deserialize(o);
 }
 
 bsp::ClockSignalConfig::ClockSignalConfig(RCC_ClkInitTypeDef const &o)
@@ -18,13 +94,25 @@ bsp::ClockSignalConfig::ClockSignalConfig(RCC_ClkInitTypeDef const &o)
 
 ClockSignalConfig &ClockSignalConfig::operator=(RCC_ClkInitTypeDef const &o)
 {
+	Deserialize(o);
 	return *this;
 }
 
 ClockSignalConfig::operator RCC_ClkInitTypeDef() const
 {
 	RCC_ClkInitTypeDef o;
+	Serialize(o);
 	return o;
+}
+
+void bsp::ClockSignalConfig::SelectAllClockType()
+{
+	_clock_type = ClockType::SYSCLK
+		| ClockType::HCLK
+		| ClockType::D1PCLK1
+		| ClockType::PCLK1
+		| ClockType::PCLK2
+		| ClockType::D3PCLK1;
 }
 
 bsp::ClockSignalConfig::ClockType operator|(
@@ -37,3 +125,4 @@ bsp::ClockSignalConfig::ClockType operator|(
 	T result = static_cast<T>(left) | static_cast<T>(right);
 	return static_cast<ClockSignalConfig::ClockType>(result);
 }
+
--- atk-stm32h743-bsp/private_src/clock/ClockSignalConfig.h
+++ atk-stm32h743-bsp/private_src/clock/ClockSignalConfig.h
@@ -3,10 +3,163 @@
 
 namespace bsp
 {
+	class Apb1ClkConfig
+	{
+	public:
+		enum class InputDivider
+		{
+			DIV1 = RCC_APB1_DIV1,
+			DIV2 = RCC_APB1_DIV2,
+			DIV4 = RCC_APB1_DIV4,
+			DIV8 = RCC_APB1_DIV8,
+			DIV16 = RCC_APB1_DIV16,
+		};
+
+		InputDivider _input_divider;
+
+		void Serialize(RCC_ClkInitTypeDef &o) const;
+		void Deserialize(RCC_ClkInitTypeDef const &o);
+	};
+
+	class Apb2ClkConfig
+	{
+	public:
+		enum class InputDivider
+		{
+			DIV1 = RCC_APB2_DIV1,
+			DIV2 = RCC_APB2_DIV2,
+			DIV4 = RCC_APB2_DIV4,
+			DIV8 = RCC_APB2_DIV8,
+			DIV16 = RCC_APB2_DIV16,
+		};
+
+		/// <summary>
+		///		为 APB2 或者叫 PCLK2 选择分频系数。
+		/// </summary>
+		InputDivider _input_divider;
+
+		void Serialize(RCC_ClkInitTypeDef &o) const;
+		void Deserialize(RCC_ClkInitTypeDef const &o);
+	};
+
+	class Apb3ClkConfig
+	{
+	public:
+		enum class InputDivider
+		{
+			DIV1 = RCC_APB3_DIV1,
+			DIV2 = RCC_APB3_DIV2,
+			DIV4 = RCC_APB3_DIV4,
+			DIV8 = RCC_APB3_DIV8,
+			DIV16 = RCC_APB3_DIV16,
+		};
+
+		InputDivider _input_divider;
+
+		void Serialize(RCC_ClkInitTypeDef &o) const;
+		void Deserialize(RCC_ClkInitTypeDef const &o);
+	};
+
+	class Apb4ClkConfig
+	{
+	public:
+		enum class InputDivider
+		{
+			DIV1 = RCC_APB4_DIV1,
+			DIV2 = RCC_APB4_DIV2,
+			DIV4 = RCC_APB4_DIV4,
+			DIV8 = RCC_APB4_DIV8,
+			DIV16 = RCC_APB4_DIV16,
+		};
+
+		InputDivider _input_divider;
+
+		void Serialize(RCC_ClkInitTypeDef &o) const;
+		void Deserialize(RCC_ClkInitTypeDef const &o);
+	};
+
+	class HclkConfig
+	{
+	public:
+		enum class InputDivider
+		{
+			DIV1 = RCC_HCLK_DIV1,
+			DIV2 = RCC_HCLK_DIV2,
+			DIV4 = RCC_HCLK_DIV4,
+			DIV8 = RCC_HCLK_DIV8,
+			DIV16 = RCC_HCLK_DIV16,
+			DIV64 = RCC_HCLK_DIV64,
+			DIV128 = RCC_HCLK_DIV128,
+			DIV256 = RCC_HCLK_DIV256,
+			DIV512 = RCC_HCLK_DIV512,
+		};
+
+		/// <summary>
+		///		HCLK 输入端的分频器。
+		/// </summary>
+		InputDivider _input_divider;
+
+		#pragma region 子时钟
+		Apb1ClkConfig _apb1clk_config;
+		Apb2ClkConfig _apb2clk_config;
+		Apb3ClkConfig _apb3clk_config;
+		Apb4ClkConfig _apb4clk_config;
+		#pragma endregion
+
+		void Serialize(RCC_ClkInitTypeDef &o) const;
+		void Deserialize(RCC_ClkInitTypeDef const &o);
+	};
+
+	/// <summary>
+	///		SYSCLK
+	///		* 直接连着时钟源的一个时钟信号。
+	/// </summary>
+	class SystemClockConfig
+	{
+	public:
+		enum class ClockSource
+		{
+			CSI = RCC_SYSCLKSOURCE_CSI,
+			HSI = RCC_SYSCLKSOURCE_HSI,
+			HSE = RCC_SYSCLKSOURCE_HSE,
+			PLLCLK = RCC_SYSCLKSOURCE_PLLCLK,
+		};
+
+		/// <summary>
+		///		选择 SYSCLK 的输入时钟源
+		/// </summary>
+		ClockSource _clock_source;
+
+		enum class OutputDivider
+		{
+			DIV1 = RCC_SYSCLK_DIV1,
+			DIV2 = RCC_SYSCLK_DIV2,
+			DIV4 = RCC_SYSCLK_DIV4,
+			DIV8 = RCC_SYSCLK_DIV8,
+			DIV16 = RCC_SYSCLK_DIV16,
+			DIV64 = RCC_SYSCLK_DIV64,
+			DIV128 = RCC_SYSCLK_DIV128,
+			DIV256 = RCC_SYSCLK_DIV256,
+			DIV512 = RCC_SYSCLK_DIV512,
+		};
+
+		/// <summary>
+		///		SYSCLK 输出的分频系数
+		/// </summary>
+		OutputDivider _output_divider;
+
+		#pragma region 子时钟信号
+		HclkConfig _hclk_config;
+		#pragma endregion
+
+		void Serialize(RCC_ClkInitTypeDef &o) const;
+		void Deserialize(RCC_ClkInitTypeDef const &o);
+	};
+
 	class ClockSignalConfig
 	{
 	public:
-		ClockSignalConfig();
+		ClockSignalConfig() = default;
 		ClockSignalConfig(RCC_ClkInitTypeDef const &o);
 		ClockSignalConfig &operator=(RCC_ClkInitTypeDef const &o);
 		operator RCC_ClkInitTypeDef() const;
@@ -27,115 +180,11 @@ namespace bsp
 		ClockType _clock_type;
 
 		/// <summary>
-		///		SysTick 可选的时钟源。
+		///		选中所有时钟类型。
 		/// </summary>
-		enum class SysTickClockSource
-		{
-			CSI = RCC_SYSCLKSOURCE_CSI,
-			HSI = RCC_SYSCLKSOURCE_HSI,
-			HSE = RCC_SYSCLKSOURCE_HSE,
-			PLLCLK = RCC_SYSCLKSOURCE_PLLCLK,
-		};
+		void SelectAllClockType();
 
-		/// <summary>
-		///		为 SysTick 选择时钟源。
-		/// </summary>
-		SysTickClockSource _systick_clock_source;
-
-		/// <summary>
-		///		SysTick 可选的分频系数
-		/// </summary>
-		enum class SysTickDivider
-		{
-			DIV1 = RCC_SYSCLK_DIV1,
-			DIV2 = RCC_SYSCLK_DIV2,
-			DIV4 = RCC_SYSCLK_DIV4,
-			DIV8 = RCC_SYSCLK_DIV8,
-			DIV16 = RCC_SYSCLK_DIV16,
-			DIV64 = RCC_SYSCLK_DIV64,
-			DIV128 = RCC_SYSCLK_DIV128,
-			DIV256 = RCC_SYSCLK_DIV256,
-			DIV512 = RCC_SYSCLK_DIV512,
-		};
-
-		/// <summary>
-		///		为 SysTick 选择分频系数。
-		/// </summary>
-		SysTickDivider systick_divider;
-
-		/// <summary>
-		///		以 HCLK 为时钟源的时钟信号可以选择让 HCLK 多少分频后再进来。
-		/// </summary>
-		enum class HclkDivider
-		{
-			DIV1 = RCC_HCLK_DIV1,
-			DIV2 = RCC_HCLK_DIV2,
-			DIV4 = RCC_HCLK_DIV4,
-			DIV8 = RCC_HCLK_DIV8,
-			DIV16 = RCC_HCLK_DIV16,
-			DIV64 = RCC_HCLK_DIV64,
-			DIV128 = RCC_HCLK_DIV128,
-			DIV256 = RCC_HCLK_DIV256,
-			DIV512 = RCC_HCLK_DIV512,
-		};
-
-		HclkDivider _ahb_clk_divider;
-
-		/// <summary>
-		///		APB3 CLK 可选的分频系数。
-		/// </summary>
-		enum class Apb3Divider
-		{
-			DIV1 = RCC_APB3_DIV1,
-			DIV2 = RCC_APB3_DIV2,
-			DIV4 = RCC_APB3_DIV4,
-			DIV8 = RCC_APB3_DIV8,
-			DIV16 = RCC_APB3_DIV16,
-		};
-
-		/// <summary>
-		///		为 APB3 CLK 或者叫 D1PCLK1 选择分频系数。
-		/// </summary>
-		Apb3Divider _apb3_clk_divider;
-
-		enum class Apb1Divider
-		{
-			DIV1 = RCC_APB1_DIV1,
-			DIV2 = RCC_APB1_DIV2,
-			DIV4 = RCC_APB1_DIV4,
-			DIV8 = RCC_APB1_DIV8,
-			DIV16 = RCC_APB1_DIV16,
-		};
-
-		/// <summary>
-		///		为 APB1 CLK 或者叫 PCLK1 选择分频系数。
-		/// </summary>
-		Apb1Divider _apb1_clk_divider;
-
-		enum class Apb2Divider
-		{
-			DIV1 = RCC_APB2_DIV1,
-			DIV2 = RCC_APB2_DIV2,
-			DIV4 = RCC_APB2_DIV4,
-			DIV8 = RCC_APB2_DIV8,
-			DIV16 = RCC_APB2_DIV16,
-		};
-
-		/// <summary>
-		///		为 APB2 或者叫 PCLK2 选择分频系数。
-		/// </summary>
-		Apb2Divider _apb2_clk_divider;
-
-		enum class Apb4Divider
-		{
-			DIV1 = RCC_APB4_DIV1,
-			DIV2 = RCC_APB4_DIV2,
-			DIV4 = RCC_APB4_DIV4,
-			DIV8 = RCC_APB4_DIV8,
-			DIV16 = RCC_APB4_DIV16,
-		};
-
-		Apb4Divider _apb4_clk_divider;
+		SystemClockConfig _system_clk_config;
 
 		/// <summary>
 		///		读写 flash 时要延迟多少个 CPU 时钟周期。
@@ -158,6 +207,9 @@ namespace bsp
 		};
 
 		FlashLatency _flash_latency = FlashLatency::Latency2;
+
+		void Serialize(RCC_ClkInitTypeDef &o) const;
+		void Deserialize(RCC_ClkInitTypeDef const &o);
 	};
 }
 
