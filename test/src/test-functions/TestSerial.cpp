4afeac0a26b72830dd4ad313427ba60bd306be6f
hjc2000 <hjc.666@foxmail.com>
2024-05-31 13:35:28 +0800

    void TestSerial()

--- test/src/test-functions/TestSerial.cpp
+++ test/src/test-functions/TestSerial.cpp
@@ -7,12 +7,11 @@ void TestSerial()
 	BSP::Delayer().Delay(std::chrono::seconds { 1 });
 	BSP::RedDigitalLed().TurnOn();
 	BSP::Serial().Begin(115200);
-	std::unique_ptr<uint8_t[]> buffer { new uint8_t[128] { 72, 72, 72, } };
+	std::unique_ptr<uint8_t[]> buffer { new uint8_t[128] { } };
 	while (true)
 	{
-		//int32_t have_read = BSP::Serial().Read(buffer.get(), 0, 128);
+		int32_t have_read = BSP::Serial().Read(buffer.get(), 0, 128);
 		BSP::GreenDigitalLed().Toggle();
-		BSP::Serial().Write(buffer.get(), 0, 3);
-		BSP::Delayer().Delay(std::chrono::seconds { 1 });
+		BSP::Serial().Write(buffer.get(), 0, have_read);
 	}
 }
