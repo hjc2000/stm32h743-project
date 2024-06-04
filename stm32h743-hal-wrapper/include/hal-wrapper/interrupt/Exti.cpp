97e5cac67728d3feaf8dfcee0949ba0580c94e88
hjc2000 <hjc.666@foxmail.com>
2024-06-04 10:21:53 +0800

    hal::

--- stm32h743-hal-wrapper/include/hal-wrapper/interrupt/Exti.cpp
+++ stm32h743-hal-wrapper/include/hal-wrapper/interrupt/Exti.cpp
@@ -1,38 +1,36 @@
 #include"Exti.h"
 #include<hal-wrapper/interrupt/Interrupt.h>
-
-using namespace hal;
+#include<stdexcept>
 
 /// <summary>
 ///		EXTI 中断函数的定义。
-///		* 记得在启动文件中删除这些符号的 weak 版本，否则链接器有可能优化掉这里的处理函数，
-///		  转而使用空操作的 weak 版本。
+///		* 记得在启动文件中删除这些符号的 weak 版本。
 /// </summary>
 extern "C"
 {
 	void EXTI0_IRQHandler()
 	{
-		HAL_GPIO_EXTI_IRQHandler((uint16_t)GpioPin::Pin0);
+		HAL_GPIO_EXTI_IRQHandler((uint16_t)hal::GpioPin::Pin0);
 	}
 
 	void EXTI1_IRQHandler()
 	{
-		HAL_GPIO_EXTI_IRQHandler((uint16_t)GpioPin::Pin1);
+		HAL_GPIO_EXTI_IRQHandler((uint16_t)hal::GpioPin::Pin1);
 	}
 
 	void EXTI2_IRQHandler()
 	{
-		HAL_GPIO_EXTI_IRQHandler((uint16_t)GpioPin::Pin2);
+		HAL_GPIO_EXTI_IRQHandler((uint16_t)hal::GpioPin::Pin2);
 	}
 
 	void EXTI3_IRQHandler()
 	{
-		HAL_GPIO_EXTI_IRQHandler((uint16_t)GpioPin::Pin3);
+		HAL_GPIO_EXTI_IRQHandler((uint16_t)hal::GpioPin::Pin3);
 	}
 
 	void EXTI4_IRQHandler()
 	{
-		HAL_GPIO_EXTI_IRQHandler((uint16_t)GpioPin::Pin4);
+		HAL_GPIO_EXTI_IRQHandler((uint16_t)hal::GpioPin::Pin4);
 	}
 
 	/// <summary>
@@ -43,47 +41,47 @@ extern "C"
 	{
 		switch (pin)
 		{
-		case (uint16_t)GpioPin::Pin0:
+		case (uint16_t)hal::GpioPin::Pin0:
 			{
-				if (Exti::Instance()._on_exti0_interrupt)
+				if (hal::Exti::Instance()._on_exti0_interrupt)
 				{
-					Exti::Instance()._on_exti0_interrupt();
+					hal::Exti::Instance()._on_exti0_interrupt();
 				}
 
 				break;
 			}
-		case (uint16_t)GpioPin::Pin1:
+		case (uint16_t)hal::GpioPin::Pin1:
 			{
-				if (Exti::Instance()._on_exti1_interrupt)
+				if (hal::Exti::Instance()._on_exti1_interrupt)
 				{
-					Exti::Instance()._on_exti1_interrupt();
+					hal::Exti::Instance()._on_exti1_interrupt();
 				}
 
 				break;
 			}
-		case (uint16_t)GpioPin::Pin2:
+		case (uint16_t)hal::GpioPin::Pin2:
 			{
-				if (Exti::Instance()._on_exti2_interrupt)
+				if (hal::Exti::Instance()._on_exti2_interrupt)
 				{
-					Exti::Instance()._on_exti2_interrupt();
+					hal::Exti::Instance()._on_exti2_interrupt();
 				}
 
 				break;
 			}
-		case (uint16_t)GpioPin::Pin3:
+		case (uint16_t)hal::GpioPin::Pin3:
 			{
-				if (Exti::Instance()._on_exti3_interrupt)
+				if (hal::Exti::Instance()._on_exti3_interrupt)
 				{
-					Exti::Instance()._on_exti3_interrupt();
+					hal::Exti::Instance()._on_exti3_interrupt();
 				}
 
 				break;
 			}
-		case (uint16_t)GpioPin::Pin4:
+		case (uint16_t)hal::GpioPin::Pin4:
 			{
-				if (Exti::Instance()._on_exti4_interrupt)
+				if (hal::Exti::Instance()._on_exti4_interrupt)
 				{
-					Exti::Instance()._on_exti4_interrupt();
+					hal::Exti::Instance()._on_exti4_interrupt();
 				}
 
 				break;
@@ -96,39 +94,39 @@ extern "C"
 	}
 }// extern "C"
 
-void Exti::UseLine(std::function<void()> callback, uint32_t line)
+void hal::Exti::UseLine(std::function<void()> callback, hal::GpioPin pin)
 {
-	switch (line)
+	switch (pin)
 	{
-	case 0:
+	case hal::GpioPin::Pin0:
 		{
 			_on_exti0_interrupt = callback;
 			hal::Interrupt::SetPriority(IRQn_Type::EXTI0_IRQn, 4, 0);
 			hal::Interrupt::EnableIRQ(IRQn_Type::EXTI0_IRQn);
 			break;
 		}
-	case 1:
+	case hal::GpioPin::Pin1:
 		{
 			_on_exti1_interrupt = callback;
 			hal::Interrupt::SetPriority(IRQn_Type::EXTI1_IRQn, 4, 0);
 			hal::Interrupt::EnableIRQ(IRQn_Type::EXTI1_IRQn);
 			break;
 		}
-	case 2:
+	case hal::GpioPin::Pin2:
 		{
 			_on_exti2_interrupt = callback;
 			hal::Interrupt::SetPriority(IRQn_Type::EXTI2_IRQn, 4, 0);
 			hal::Interrupt::EnableIRQ(IRQn_Type::EXTI2_IRQn);
 			break;
 		}
-	case 3:
+	case hal::GpioPin::Pin3:
 		{
 			_on_exti3_interrupt = callback;
 			hal::Interrupt::SetPriority(IRQn_Type::EXTI3_IRQn, 4, 0);
 			hal::Interrupt::EnableIRQ(IRQn_Type::EXTI3_IRQn);
 			break;
 		}
-	case 4:
+	case hal::GpioPin::Pin4:
 		{
 			_on_exti4_interrupt = callback;
 			hal::Interrupt::SetPriority(IRQn_Type::EXTI4_IRQn, 4, 0);
@@ -137,40 +135,40 @@ void Exti::UseLine(std::function<void()> callback, uint32_t line)
 		}
 	default:
 		{
-			break;
+			throw std::invalid_argument { "pin 超出范围。" };
 		}
 	}
 }
 
-void Exti::UnuseLine(uint32_t line)
+void hal::Exti::UnuseLine(hal::GpioPin pin)
 {
-	switch (line)
+	switch (pin)
 	{
-	case 0:
+	case hal::GpioPin::Pin0:
 		{
 			hal::Interrupt::DisableIRQ(IRQn_Type::EXTI0_IRQn);
 			_on_exti0_interrupt = nullptr;
 			break;
 		}
-	case 1:
+	case hal::GpioPin::Pin1:
 		{
 			hal::Interrupt::DisableIRQ(IRQn_Type::EXTI1_IRQn);
 			_on_exti1_interrupt = nullptr;
 			break;
 		}
-	case 2:
+	case hal::GpioPin::Pin2:
 		{
 			hal::Interrupt::DisableIRQ(IRQn_Type::EXTI2_IRQn);
 			_on_exti2_interrupt = nullptr;
 			break;
 		}
-	case 3:
+	case hal::GpioPin::Pin3:
 		{
 			hal::Interrupt::DisableIRQ(IRQn_Type::EXTI3_IRQn);
 			_on_exti3_interrupt = nullptr;
 			break;
 		}
-	case 4:
+	case hal::GpioPin::Pin4:
 		{
 			hal::Interrupt::DisableIRQ(IRQn_Type::EXTI4_IRQn);
 			_on_exti4_interrupt = nullptr;
@@ -178,7 +176,7 @@ void Exti::UnuseLine(uint32_t line)
 		}
 	default:
 		{
-			break;
+			throw std::invalid_argument { "pin 超出范围。" };
 		}
 	}
 }
--- stm32h743-hal-wrapper/include/hal-wrapper/interrupt/Exti.h
+++ stm32h743-hal-wrapper/include/hal-wrapper/interrupt/Exti.h
@@ -1,7 +1,11 @@
 #pragma once
 #include<functional>
 #include<hal-wrapper/peripheral/gpio/GpioPort.h>
-#include<stdint.h>
+
+extern "C"
+{
+	void HAL_GPIO_EXTI_Callback(uint16_t pin);
+}
 
 namespace hal
 {
@@ -9,7 +13,6 @@ namespace hal
 	///		* 所有 GPIO 端口的 pin0 连接到中断管理器的 line0，
 	///		  所有 GPIO 端口的 pin1 连接到中断管理器的 line1，
 	///		  以此类推。
-	/// 
 	///		* 例如同样都是 pin0，可能来自 GPIOA，GPIOB,...... 等。所有
 	///		  这些端口的 pin0 都通过一个多路选择开关连接到中断管理器的 line0 上，
 	///		  这个多路选择开关只能同时选择一条路导通。
@@ -35,34 +38,17 @@ namespace hal
 		}
 
 		/// <summary>
-		///		清除某个 GPIO 引脚上的中断挂起标志。
-		/// </summary>
-		/// <param name="line"></param>
-		void ClearGpioInterruptPending(uint32_t line)
-		{
-			__HAL_GPIO_EXTI_CLEAR_IT(line);
-		}
-
-		/// <summary>
-		///		检查指定的线是否有中断标志正被挂起。
-		/// </summary>
-		bool HasInterruptFlag(uint32_t line)
-		{
-			return __HAL_GPIO_EXTI_GET_IT(line);
-		}
-
-		/// <summary>
 		///		使用一条外部中断线。
 		/// </summary>
 		/// <param name="callback"></param>
-		/// <param name="line"></param>
-		void UseLine(std::function<void()> callback, uint32_t line);
+		/// <param name="pin"></param>
+		void UseLine(std::function<void()> callback, hal::GpioPin pin);
 
 		/// <summary>
 		///		取消使用一条外部中断线。
 		///		* 会先禁用对应的线的中断，然后将回调函数设为 nullptr。
 		/// </summary>
-		/// <param name="line"></param>
-		void UnuseLine(uint32_t line);
+		/// <param name="pin"></param>
+		void UnuseLine(hal::GpioPin pin);
 	};
 }
