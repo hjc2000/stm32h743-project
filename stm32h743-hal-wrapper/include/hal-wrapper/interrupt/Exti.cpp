3547a5821c87fe69a96fa72ace9203cee7432b85
hjc2000 <hjc.666@foxmail.com>
2024-06-04 10:41:35 +0800

    void HAL_GPIO_EXTI_Callback(uint16_t pin)

--- stm32h743-hal-wrapper/include/hal-wrapper/interrupt/Exti.cpp
+++ stm32h743-hal-wrapper/include/hal-wrapper/interrupt/Exti.cpp
@@ -2,6 +2,8 @@
 #include<hal-wrapper/interrupt/Interrupt.h>
 #include<stdexcept>
 
+using namespace hal;
+
 /// <summary>
 ///		EXTI 中断函数的定义。
 ///		* 记得在启动文件中删除这些符号的 weak 版本。
@@ -10,78 +12,79 @@ extern "C"
 {
 	void EXTI0_IRQHandler()
 	{
-		HAL_GPIO_EXTI_IRQHandler((uint16_t)hal::GpioPin::Pin0);
+		HAL_GPIO_EXTI_IRQHandler((uint16_t)GpioPin::Pin0);
 	}
 
 	void EXTI1_IRQHandler()
 	{
-		HAL_GPIO_EXTI_IRQHandler((uint16_t)hal::GpioPin::Pin1);
+		HAL_GPIO_EXTI_IRQHandler((uint16_t)GpioPin::Pin1);
 	}
 
 	void EXTI2_IRQHandler()
 	{
-		HAL_GPIO_EXTI_IRQHandler((uint16_t)hal::GpioPin::Pin2);
+		HAL_GPIO_EXTI_IRQHandler((uint16_t)GpioPin::Pin2);
 	}
 
 	void EXTI3_IRQHandler()
 	{
-		HAL_GPIO_EXTI_IRQHandler((uint16_t)hal::GpioPin::Pin3);
+		HAL_GPIO_EXTI_IRQHandler((uint16_t)GpioPin::Pin3);
 	}
 
 	void EXTI4_IRQHandler()
 	{
-		HAL_GPIO_EXTI_IRQHandler((uint16_t)hal::GpioPin::Pin4);
+		HAL_GPIO_EXTI_IRQHandler((uint16_t)GpioPin::Pin4);
 	}
 
 	/// <summary>
 	///		重写 HAL 库中的 weak 版本
 	/// </summary>
-	/// <param name="GPIO_Pin"></param>
+	/// <param name="pin"></param>
 	void HAL_GPIO_EXTI_Callback(uint16_t pin)
 	{
-		switch (pin)
+		// 这里传进来的是 GpioPin 类型，需要手动强制转换一下。
+		switch (static_cast<GpioPin>(pin))
 		{
-		case (uint16_t)hal::GpioPin::Pin0:
+		case GpioPin::Pin0:
 			{
-				if (hal::Exti::Instance()._on_exti0_interrupt)
+				if (Exti::Instance()._on_exti0_interrupt)
 				{
-					hal::Exti::Instance()._on_exti0_interrupt();
+					Exti::Instance()._on_exti0_interrupt();
 				}
 
 				break;
 			}
-		case (uint16_t)hal::GpioPin::Pin1:
+		case GpioPin::Pin1:
 			{
-				if (hal::Exti::Instance()._on_exti1_interrupt)
+				if (Exti::Instance()._on_exti1_interrupt)
 				{
-					hal::Exti::Instance()._on_exti1_interrupt();
+					Exti::Instance()._on_exti1_interrupt();
 				}
 
 				break;
 			}
-		case (uint16_t)hal::GpioPin::Pin2:
+		case GpioPin::Pin2:
 			{
-				if (hal::Exti::Instance()._on_exti2_interrupt)
+				if (Exti::Instance()._on_exti2_interrupt)
 				{
-					hal::Exti::Instance()._on_exti2_interrupt();
+					Exti::Instance()._on_exti2_interrupt();
 				}
 
 				break;
 			}
-		case (uint16_t)hal::GpioPin::Pin3:
+		case GpioPin::Pin3:
 			{
-				if (hal::Exti::Instance()._on_exti3_interrupt)
+				if (Exti::Instance()._on_exti3_interrupt)
 				{
-					hal::Exti::Instance()._on_exti3_interrupt();
+					Exti::Instance()._on_exti3_interrupt();
 				}
 
 				break;
 			}
-		case (uint16_t)hal::GpioPin::Pin4:
+		case GpioPin::Pin4:
 			{
-				if (hal::Exti::Instance()._on_exti4_interrupt)
+				if (Exti::Instance()._on_exti4_interrupt)
 				{
-					hal::Exti::Instance()._on_exti4_interrupt();
+					Exti::Instance()._on_exti4_interrupt();
 				}
 
 				break;
@@ -94,43 +97,43 @@ extern "C"
 	}
 }// extern "C"
 
-void hal::Exti::UseLine(std::function<void()> callback, hal::GpioPin pin)
+void Exti::UseLine(std::function<void()> callback, GpioPin pin)
 {
 	switch (pin)
 	{
-	case hal::GpioPin::Pin0:
+	case GpioPin::Pin0:
 		{
 			_on_exti0_interrupt = callback;
-			hal::Interrupt::SetPriority(IRQn_Type::EXTI0_IRQn, 4, 0);
-			hal::Interrupt::EnableIRQ(IRQn_Type::EXTI0_IRQn);
+			Interrupt::SetPriority(IRQn_Type::EXTI0_IRQn, 4, 0);
+			Interrupt::EnableIRQ(IRQn_Type::EXTI0_IRQn);
 			break;
 		}
-	case hal::GpioPin::Pin1:
+	case GpioPin::Pin1:
 		{
 			_on_exti1_interrupt = callback;
-			hal::Interrupt::SetPriority(IRQn_Type::EXTI1_IRQn, 4, 0);
-			hal::Interrupt::EnableIRQ(IRQn_Type::EXTI1_IRQn);
+			Interrupt::SetPriority(IRQn_Type::EXTI1_IRQn, 4, 0);
+			Interrupt::EnableIRQ(IRQn_Type::EXTI1_IRQn);
 			break;
 		}
-	case hal::GpioPin::Pin2:
+	case GpioPin::Pin2:
 		{
 			_on_exti2_interrupt = callback;
-			hal::Interrupt::SetPriority(IRQn_Type::EXTI2_IRQn, 4, 0);
-			hal::Interrupt::EnableIRQ(IRQn_Type::EXTI2_IRQn);
+			Interrupt::SetPriority(IRQn_Type::EXTI2_IRQn, 4, 0);
+			Interrupt::EnableIRQ(IRQn_Type::EXTI2_IRQn);
 			break;
 		}
-	case hal::GpioPin::Pin3:
+	case GpioPin::Pin3:
 		{
 			_on_exti3_interrupt = callback;
-			hal::Interrupt::SetPriority(IRQn_Type::EXTI3_IRQn, 4, 0);
-			hal::Interrupt::EnableIRQ(IRQn_Type::EXTI3_IRQn);
+			Interrupt::SetPriority(IRQn_Type::EXTI3_IRQn, 4, 0);
+			Interrupt::EnableIRQ(IRQn_Type::EXTI3_IRQn);
 			break;
 		}
-	case hal::GpioPin::Pin4:
+	case GpioPin::Pin4:
 		{
 			_on_exti4_interrupt = callback;
-			hal::Interrupt::SetPriority(IRQn_Type::EXTI4_IRQn, 4, 0);
-			hal::Interrupt::EnableIRQ(IRQn_Type::EXTI4_IRQn);
+			Interrupt::SetPriority(IRQn_Type::EXTI4_IRQn, 4, 0);
+			Interrupt::EnableIRQ(IRQn_Type::EXTI4_IRQn);
 			break;
 		}
 	default:
@@ -140,37 +143,37 @@ void hal::Exti::UseLine(std::function<void()> callback, hal::GpioPin pin)
 	}
 }
 
-void hal::Exti::UnuseLine(hal::GpioPin pin)
+void Exti::UnuseLine(GpioPin pin)
 {
 	switch (pin)
 	{
-	case hal::GpioPin::Pin0:
+	case GpioPin::Pin0:
 		{
-			hal::Interrupt::DisableIRQ(IRQn_Type::EXTI0_IRQn);
+			Interrupt::DisableIRQ(IRQn_Type::EXTI0_IRQn);
 			_on_exti0_interrupt = nullptr;
 			break;
 		}
-	case hal::GpioPin::Pin1:
+	case GpioPin::Pin1:
 		{
-			hal::Interrupt::DisableIRQ(IRQn_Type::EXTI1_IRQn);
+			Interrupt::DisableIRQ(IRQn_Type::EXTI1_IRQn);
 			_on_exti1_interrupt = nullptr;
 			break;
 		}
-	case hal::GpioPin::Pin2:
+	case GpioPin::Pin2:
 		{
-			hal::Interrupt::DisableIRQ(IRQn_Type::EXTI2_IRQn);
+			Interrupt::DisableIRQ(IRQn_Type::EXTI2_IRQn);
 			_on_exti2_interrupt = nullptr;
 			break;
 		}
-	case hal::GpioPin::Pin3:
+	case GpioPin::Pin3:
 		{
-			hal::Interrupt::DisableIRQ(IRQn_Type::EXTI3_IRQn);
+			Interrupt::DisableIRQ(IRQn_Type::EXTI3_IRQn);
 			_on_exti3_interrupt = nullptr;
 			break;
 		}
-	case hal::GpioPin::Pin4:
+	case GpioPin::Pin4:
 		{
-			hal::Interrupt::DisableIRQ(IRQn_Type::EXTI4_IRQn);
+			Interrupt::DisableIRQ(IRQn_Type::EXTI4_IRQn);
 			_on_exti4_interrupt = nullptr;
 			break;
 		}
