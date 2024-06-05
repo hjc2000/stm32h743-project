7994475bfc1bf3e255e08084f4e2e42ef5e56f8f
hjc2000 <hjc.666@foxmail.com>
2024-06-05 11:05:18 +0800

    void IndependentWatchDog::SetWatchDogTimeoutDuration(std::chrono::milliseconds value)

--- stm32h743-hal-wrapper/include/hal-wrapper/peripheral/independent-watch-dog/IndependentWatchDog.cpp
+++ stm32h743-hal-wrapper/include/hal-wrapper/peripheral/independent-watch-dog/IndependentWatchDog.cpp
@@ -22,35 +22,21 @@ std::chrono::milliseconds IndependentWatchDog::WatchDogTimeoutDuration() const
 
 void IndependentWatchDog::SetWatchDogTimeoutDuration(std::chrono::milliseconds value)
 {
-	/*
-	* 计数器的计数周期为：
-	*	T = 1 / CounterFreq_Hz()
-	* 单位是秒。
-	* value 化成秒是：
-	*	seconds = value / 1000
-	* 想要持续 seconds 秒，计数值是：
-	*	count = seconds / T
-	*	count = value / 1000 / T
-	*	count = value / 1000 * CounterFreq_Hz()
-	* 先乘再除可以防止截断误差
-	*	count = value * CounterFreq_Hz() / 1000
-	*	count = value * InnerClockSourceFreq_Hz() / PrescalerValue() / 1000
-	*	count * PrescalerValue() = value * InnerClockSourceFreq_Hz() / 1000
-	*
-	* count * PrescalerValue() 是具有物理意义的。分频器也可以看作是计数器，分频器计数满了，
-	* 进位给计数器，就实现分频了。看门狗的分频系数都是 2 的整数次幂，从 4 到 256，我们只需要
-	* 找出不让计数器溢出的最小分频系数。
-	*/
+	Fraction inner_clock_source_freq { InnerClockSourceFreq_Hz() };
+	Fraction inner_clock_source_interval = 1 / inner_clock_source_freq;
+	Fraction timeout_in_milliseconds = value.count();
+	Fraction timeout = timeout_in_milliseconds / 1000;
 
 	// 所需的 (分频器计数值 + 计数器计数值)
-	uint64_t total_count = value.count() * InnerClockSourceFreq_Hz() / 1000;
+	Fraction total_count = timeout / inner_clock_source_interval;
+	int64_t int_total_count = total_count.Div();
 
 	// 让计数器重装载值尽量大，溢出了就增大分频系数
 	for (uint16_t i = 2; i <= 8; i++)
 	{
 		// 从 2^2 = 4 开始，到 2^8 = 256，通过移位实现幂。i 代表的是 2 的幂
 		uint16_t prescaler_value = static_cast<uint16_t>(1 << i);
-		_config.SetReloadValue(total_count / prescaler_value);
+		_config.SetReloadValue(int_total_count / prescaler_value);
 		if (_config.ReloadValue() > 0X0FFF && i == 8)
 		{
 			// 最大分频和最大计数都无法表示这个时间，就按照能达到的最大值来。
