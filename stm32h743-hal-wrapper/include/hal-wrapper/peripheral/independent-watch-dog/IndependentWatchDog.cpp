9e88db4ffd3f9852d2a85167961610f3819b140f
hjc2000 <hjc.666@foxmail.com>
2024-06-05 09:17:21 +0800

    IndependentWatchDog

---
+++ stm32h743-hal-wrapper/include/hal-wrapper/peripheral/independent-watch-dog/IndependentWatchDog.cpp
@@ -0,0 +1,81 @@
+#include"IndependentWatchDog.h"
+
+using namespace hal;
+
+void IndependentWatchDog::Initialize()
+{
+	_handle.Instance = HardwareInstance();
+	_handle.Init = _config.Handle();
+	HAL_IWDG_Init(&_handle);
+}
+
+std::chrono::milliseconds IndependentWatchDog::WatchDogTimeoutDuration() const
+{
+	/*
+	* 设计数器计数间隔为 count_interval，单位：秒。
+	*
+	* timeout = reload_value * count_interval
+	* count_interval = 1 / count_freq
+	* count_freq = InnerClockSourceFreq_Hz / prescaler
+	*
+	* count_interval = 1 / (InnerClockSourceFreq_Hz / prescaler)
+	* count_interval = prescaler / InnerClockSourceFreq_Hz
+	* timeout = reload_value * prescaler / InnerClockSourceFreq_Hz
+	*/
+	return std::chrono::milliseconds { static_cast<int64_t>(1000) * _config.ReloadValue() * _config.GetPrescalerByUint32() / InnerClockSourceFreq_Hz() };
+}
+
+void IndependentWatchDog::SetWatchDogTimeoutDuration(std::chrono::milliseconds value)
+{
+	/*
+	* 计数器的计数周期为：
+	*	T = 1 / CounterFreq_Hz()
+	* 单位是秒。
+	* value 化成秒是：
+	*	seconds = value / 1000
+	* 想要持续 seconds 秒，计数值是：
+	*	count = seconds / T
+	*	count = value / 1000 / T
+	*	count = value / 1000 * CounterFreq_Hz()
+	* 先乘再除可以防止截断误差
+	*	count = value * CounterFreq_Hz() / 1000
+	*	count = value * InnerClockSourceFreq_Hz() / PrescalerValue() / 1000
+	*	count * PrescalerValue() = value * InnerClockSourceFreq_Hz() / 1000
+	*
+	* count * PrescalerValue() 是具有物理意义的。分频器也可以看作是计数器，分频器计数满了，
+	* 进位给计数器，就实现分频了。看门狗的分频系数都是 2 的整数次幂，从 4 到 256，我们只需要
+	* 找出不让计数器溢出的最小分频系数。
+	*/
+
+	// 所需的 (分频器计数值 + 计数器计数值)
+	uint64_t total_count = value.count() * InnerClockSourceFreq_Hz() / 1000;
+
+	// 让计数器重装载值尽量大，溢出了就增大分频系数
+	for (uint16_t i = 2; i <= 8; i++)
+	{
+		// 从 2^2 = 4 开始，到 2^8 = 256，通过移位实现幂。i 代表的是 2 的幂
+		uint16_t prescaler_value = static_cast<uint16_t>(1 << i);
+		_config.SetReloadValue(total_count / prescaler_value);
+		if (_config.ReloadValue() > 0X0FFF && i == 8)
+		{
+			// 最大分频和最大计数都无法表示这个时间，就按照能达到的最大值来。
+			_config.SetReloadValue(0X0FFF);
+			_config.SetPrescaler(IndependentWatchDogConfig::PrescalerOption::Div256);
+			break;
+		}
+
+		if (_config.ReloadValue() <= 0x0FFF)
+		{
+			// i 代表的是 2 的幂，将 i 映射到分频系数枚举值
+			_config.SetPrescalerByPow(i);
+			break;
+		}
+	}
+
+	Initialize();
+}
+
+void IndependentWatchDog::Feed()
+{
+	HAL_IWDG_Refresh(&_handle);
+}
---
+++ stm32h743-hal-wrapper/include/hal-wrapper/peripheral/independent-watch-dog/IndependentWatchDogConfig.h
@@ -0,0 +1,67 @@
+#pragma once
+#include<base/HandleWrapper.h>
+#include<hal.h>
+#include<stdint.h>
+
+namespace hal
+{
+	/// <summary>
+	///		看门狗初始化选项。
+	/// </summary>
+	class IndependentWatchDogConfig :
+		public base::HandleWrapper<IWDG_InitTypeDef>
+	{
+	private:
+		IWDG_InitTypeDef _config_handle;
+
+	public:
+		IndependentWatchDogConfig() = default;
+		IndependentWatchDogConfig(IWDG_InitTypeDef const &o);
+		IndependentWatchDogConfig &operator=(IWDG_InitTypeDef const &o);
+
+		IWDG_InitTypeDef &Handle() override;
+
+		#pragma region 分频系数
+		/// <summary>
+		///		看门狗预分频器分频系数
+		/// </summary>
+		enum class PrescalerOption
+		{
+			Div4 = IWDG_PRESCALER_4,
+			Div8 = IWDG_PRESCALER_8,
+			Div16 = IWDG_PRESCALER_16,
+			Div32 = IWDG_PRESCALER_32,
+			Div64 = IWDG_PRESCALER_64,
+			Div128 = IWDG_PRESCALER_128,
+			Div256 = IWDG_PRESCALER_256,
+		};
+
+		/// <summary>
+		///		看门狗预分频器分频系数。
+		/// </summary>
+		PrescalerOption Prescaler() const;
+		void SetPrescaler(PrescalerOption value);
+
+		/// <summary>
+		///		根据 2 的整数幂获取分频系数。例如 PrescalerOption::Div8
+		///		是 2 的 3 次幂，于是返回 3.
+		/// </summary>
+		/// <returns></returns>
+		uint8_t GetPrescalerByPow() const;
+		void SetPrescalerByPow(uint8_t pow);
+
+		/// <summary>
+		///		获取 uint32_t 类型的分频系数，分多少频，整数值就是多少。
+		/// </summary>
+		/// <returns></returns>
+		uint32_t GetPrescalerByUint32() const;
+		void SetPrescalerByUint32(uint32_t value);
+		#pragma endregion
+
+		/// <summary>
+		///		看门狗重载值。允许的范围：[0, 0x0FFF]
+		/// </summary>
+		uint32_t ReloadValue() const;
+		void SetReloadValue(uint32_t value);
+	};
+}
