9c841cb1bf7b084288d25f6342a95ea581fe4e64
hjc2000 <hjc.666@foxmail.com>
2024-06-07 10:03:34 +0800

    uint32_t CounterFrequency()

--- stm32h743-hal-wrapper/include/hal-wrapper/peripheral/universal-timer/UniversalTimer1.h
+++ stm32h743-hal-wrapper/include/hal-wrapper/peripheral/universal-timer/UniversalTimer1.h
@@ -1,6 +1,7 @@
 #pragma once
 #include<base/HandleWrapper.h>
 #include<hal.h>
+#include<hal-wrapper/clock/ClockSignal.h>
 #include<hal-wrapper/peripheral/universal-timer/UniversalTimerConfig.h>
 
 extern "C"
@@ -35,6 +36,31 @@ namespace hal
 
 
 		TIM_HandleTypeDef &Handle() override;
+
+		/// <summary>
+		///		输入到分频器的时钟信号的频率
+		/// </summary>
+		/// <returns></returns>
+		uint32_t PrescalerInputClockSignalFrequency()
+		{
+			hal::ClockSignalConfig config = hal::ClockSignal::GetConfig();
+			uint32_t pclk1_freq = hal::ClockSignal::Pclk1Freq();
+			if (config._system_clk_config._hclk_config._apb1clk_config._input_divider == hal::Apb1ClkConfig::InputDivider::DIV1)
+			{
+				return pclk1_freq;
+			}
+
+			return pclk1_freq * 2;
+		}
+
+		/// <summary>
+		///		分频器的输出，输入到定时器的计数器中的时钟频率。
+		/// </summary>
+		/// <returns></returns>
+		uint32_t CounterFrequency()
+		{
+			return PrescalerInputClockSignalFrequency() / (_config.Prescaler() + 1);
+		}
 
 		HAL_TIM_ActiveChannel ActiveChannel()
 		{
