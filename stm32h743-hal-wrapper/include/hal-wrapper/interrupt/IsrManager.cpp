c366cd3ed1dfd5c43c14d1f25185277bb474a998
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-06-11 21:56:54 +0800

    class InterruptSwitch :

--- stm32h743-hal-wrapper/include/hal-wrapper/interrupt/IsrManager.cpp
+++ stm32h743-hal-wrapper/include/hal-wrapper/interrupt/IsrManager.cpp
@@ -4,19 +4,33 @@
 using namespace hal;
 using namespace bsp;
 
-void hal::InterruptSwitch::DisableInterrupt(uint32_t irq) noexcept
-{
-	hal::Interrupt::DisableIRQ(static_cast<IRQn_Type>(irq));
-}
-
-void hal::InterruptSwitch::EnableInterrupt(uint32_t irq) noexcept
-{
-	hal::Interrupt::EnableIRQ(static_cast<IRQn_Type>(irq));
-}
-
 bsp::IsrManager &hal::GetIsrManager()
 {
-	static bsp::IsrManager manager { hal::InterruptSwitch::Instance() };
+	class InterruptSwitch :
+		public bsp::IInterruptSwitch
+	{
+	private:
+		InterruptSwitch() = default;
+
+	public:
+		static InterruptSwitch &Instance()
+		{
+			static InterruptSwitch o;
+			return o;
+		}
+
+		void DisableInterrupt(uint32_t irq) noexcept override
+		{
+			hal::Interrupt::DisableIRQ(static_cast<IRQn_Type>(irq));
+		}
+
+		void EnableInterrupt(uint32_t irq) noexcept override
+		{
+			hal::Interrupt::EnableIRQ(static_cast<IRQn_Type>(irq));
+		}
+	};
+
+	static bsp::IsrManager manager { InterruptSwitch::Instance() };
 	return manager;
 }
 
--- stm32h743-hal-wrapper/include/hal-wrapper/interrupt/IsrManager.h
+++ stm32h743-hal-wrapper/include/hal-wrapper/interrupt/IsrManager.h
@@ -3,22 +3,5 @@
 
 namespace hal
 {
-	class InterruptSwitch :
-		public bsp::IInterruptSwitch
-	{
-	private:
-		InterruptSwitch() = default;
-
-	public:
-		static InterruptSwitch &Instance()
-		{
-			static InterruptSwitch o;
-			return o;
-		}
-
-		void DisableInterrupt(uint32_t irq) noexcept override;
-		void EnableInterrupt(uint32_t irq) noexcept override;
-	};
-
 	bsp::IsrManager &GetIsrManager();
 }
