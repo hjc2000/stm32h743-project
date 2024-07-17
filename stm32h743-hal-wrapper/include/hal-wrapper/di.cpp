4055206d8731033ce1324d2219eb7be83e6f8389
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-18 00:02:42 +0800

    namespace task

--- stm32h743-hal-wrapper/include/hal-wrapper/di.cpp
+++ stm32h743-hal-wrapper/include/hal-wrapper/di.cpp
@@ -1,7 +1,10 @@
 #include <hal-wrapper/clock/SysTickClock.h>
 #include <task/di.h>
 
-bsp::ISysTick &task::DI_SysTick()
+namespace task
 {
-	return hal::SysTickClock::Instance();
+	bsp::ISysTick &DI_SysTick()
+	{
+		return hal::SysTickClock::Instance();
+	}
 }
