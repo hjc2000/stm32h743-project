b84296c97127ede40ac9d28e0d162f03b4613087
hjc2000 <hjc.666@foxmail.com>
2024-07-17 11:50:46 +0800

    class Flash

--- stm32h743-hal-wrapper/include/hal-wrapper/Flash.cpp
+++ stm32h743-hal-wrapper/include/hal-wrapper/Flash.cpp
@@ -1,6 +1,49 @@
 #include "Flash.h"
 #include <stdexcept>
 
+uint32_t hal::Flash::SectorIndexToDefine(int32_t index)
+{
+	switch (index)
+	{
+	case 0:
+	{
+		return FLASH_SECTOR_0;
+	}
+	case 1:
+	{
+		return FLASH_SECTOR_1;
+	}
+	case 2:
+	{
+		return FLASH_SECTOR_2;
+	}
+	case 3:
+	{
+		return FLASH_SECTOR_3;
+	}
+	case 4:
+	{
+		return FLASH_SECTOR_4;
+	}
+	case 5:
+	{
+		return FLASH_SECTOR_5;
+	}
+	case 6:
+	{
+		return FLASH_SECTOR_6;
+	}
+	case 7:
+	{
+		return FLASH_SECTOR_7;
+	}
+	default:
+	{
+		throw std::out_of_range{"index 超出范围"};
+	}
+	}
+}
+
 void hal::Flash::Lock()
 {
 	HAL_StatusTypeDef result = HAL_FLASH_Lock();
@@ -61,6 +104,57 @@ void hal::Flash::EraseBank(int32_t bank_id)
 		msg += " 个扇区时出错。";
 		throw std::runtime_error{msg};
 	}
+
+	SCB_CleanInvalidateDCache();
+}
+
+void hal::Flash::EraseSector(int32_t bank_id, int32_t start_sector_index, int32_t sector_count)
+{
+	FLASH_EraseInitTypeDef def;
+
+	// 擦除类型为扇区擦除
+	def.TypeErase = FLASH_TYPEERASE_SECTORS;
+
+	// 选择 bank
+	switch (bank_id)
+	{
+	case 1:
+	{
+		def.Banks = FLASH_BANK_1;
+		break;
+	}
+	case 2:
+	{
+		def.Banks = FLASH_BANK_2;
+		break;
+	}
+	default:
+	{
+		throw std::invalid_argument{"非法 bank_id"};
+	}
+	}
+
+	def.Sector = SectorIndexToDefine(start_sector_index);
+	def.NbSectors = sector_count;
+
+	// 每次擦除 64 位，直到擦除整个 bank。越高的电压擦除时并行的位数就越多。
+	def.VoltageRange = FLASH_VOLTAGE_RANGE_4;
+
+	uint32_t error_sector_index;
+
+	/* 擦除到某个扇区的时候如果发生错误，会停止擦除，将扇区索引赋值给 error_sector_index，
+	 * 然后返回。
+	 */
+	HAL_StatusTypeDef result = HAL_FLASHEx_Erase(&def, &error_sector_index);
+	if (result != HAL_StatusTypeDef::HAL_OK)
+	{
+		std::string msg = "擦除第 ";
+		msg += std::to_string(error_sector_index);
+		msg += " 个扇区时出错。";
+		throw std::runtime_error{msg};
+	}
+
+	SCB_CleanInvalidateDCache();
 }
 
 uint32_t hal::Flash::ReadUInt32(size_t addr)
--- stm32h743-hal-wrapper/include/hal-wrapper/Flash.h
+++ stm32h743-hal-wrapper/include/hal-wrapper/Flash.h
@@ -30,6 +30,8 @@ namespace hal
 		}
 #pragma endregion
 
+		static uint32_t SectorIndexToDefine(int32_t index);
+
 	public:
 		Flash &Instance()
 		{
@@ -44,6 +46,12 @@ namespace hal
 		/// @note stm32h743 有 2 个 bank。典型用法是：bank1 用来存放程序，bank2 用来存放数据。
 		/// @param bank_id 要擦除的扇区的 id。例如要擦除 bank1，就传入 1，要擦除 bank2 就传入 2.
 		void EraseBank(int32_t bank_id);
+
+		/// @brief 擦除指定 bank 中从 start_sector_index 开始的 sector_count 个扇区。
+		/// @param bank_id 要擦除的扇区所在的 bank 的 id。
+		/// @param start_sector_index 要擦除的扇区的起始索引。
+		/// @param sector_count 要擦除的扇区的数量。
+		void EraseSector(int32_t bank_id, int32_t start_sector_index, int32_t sector_count);
 
 		/// @brief 读取指定地址的 32 位数据。
 		/// @param addr 相对于此 flash 储存器起始地址的地址。
