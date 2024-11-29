4c2971d4ce8063dbfd989145a47f3956fa7d6ed4
hjc2000 <hjc.666@foxmail.com>
2024-11-29 14:16:04 +0800

    Lfs::TestLittleFs();

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -29,63 +29,7 @@ int main(void)
                 {
                     DI_Serial().Open(*DICreate_ISerialOptions());
                     DI_Console().SetOutStream(base::RentedPtrFactory::Create(&DI_Serial()));
-                    Lfs::LfsFlashPort port{"internal-flash"};
-
-                    // 初始化 LittleFS 文件系统
-                    lfs_t lfs;
-                    int res = lfs_format(&lfs, &port.Port()); // 格式化文件系统
-                    if (res)
-                    {
-                        DI_Console().WriteLine("format failed: ");
-                    }
-
-                    res = lfs_mount(&lfs, &port.Port()); // 挂载文件系统
-                    if (res)
-                    {
-                        DI_Console().WriteLine("mount failed: ");
-                    }
-
-                    // 创建文件并写入数据
-                    char const *filename = "example.txt";
-                    char const *str = "Hello, LittleFS!";
-                    lfs_file_t file;
-
-                    res = lfs_file_open(&lfs, &file, filename, LFS_O_WRONLY | LFS_O_CREAT | LFS_O_TRUNC);
-                    if (res)
-                    {
-                        DI_Console().WriteLine("open failed: ");
-                    }
-
-                    res = lfs_file_write(&lfs, &file, str, strlen(str));
-                    if (res < 0)
-                    {
-                        DI_Console().WriteLine("write failed: ");
-                    }
-
-                    lfs_file_close(&lfs, &file);
-
-                    // 读取文件内容
-                    res = lfs_file_open(&lfs, &file, filename, LFS_O_RDONLY);
-                    if (res)
-                    {
-                        DI_Console().WriteLine("open failed: ");
-                    }
-
-                    char buffer[128] = {0};
-                    res = lfs_file_read(&lfs, &file, buffer, sizeof(buffer) - 1);
-                    if (res < 0)
-                    {
-                        DI_Console().WriteLine("read failed: ");
-                    }
-
-                    DI_Console().WriteLine("have read: ");
-                    DI_Console().WriteLine(buffer);
-
-                    lfs_file_close(&lfs, &file);
-
-                    // 卸载文件系统
-                    lfs_unmount(&lfs);
-
+                    Lfs::TestLittleFs();
                     while (true)
                     {
                         DI_GreenDigitalLed().Toggle();
