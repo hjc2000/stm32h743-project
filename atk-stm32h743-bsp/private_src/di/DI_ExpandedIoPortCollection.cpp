1b5ff95406b39df21b7e28d54f6e0d23498eca09
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-08 22:04:34 +0800

    enum class ExIoFunctionEnum

--- atk-stm32h743-bsp/private_src/di/DI_ExpandedIoPortCollection.cpp
+++ atk-stm32h743-bsp/private_src/di/DI_ExpandedIoPortCollection.cpp
@@ -8,6 +8,34 @@
 
 namespace
 {
+    /// @brief 开发板上的 PCF8574 各个引脚的功能。
+    enum class ExIoFunctionEnum
+    {
+        /// @brief 蜂鸣器控制引脚
+        BEEP_IO = 0,
+
+        /// @brief AP3216C中断引脚
+        AP_INT_IO = 1,
+
+        /// @brief DCMI的电源控制引脚
+        DCMI_PWDN_IO = 2,
+
+        /// @brief USB电源控制引脚
+        USB_PWR_IO = 3,
+
+        /// @brief 用户自定义使用
+        Costum = 4,
+
+        /// @brief MPU9250中断引脚
+        MPU_INT_IO = 5,
+
+        /// @brief RS485_RE引脚
+        RS485_RE_IO = 6,
+
+        /// @brief 以太网复位引脚
+        ETH_RESET_IO = 7,
+    };
+
     class Initializer
     {
     private:
