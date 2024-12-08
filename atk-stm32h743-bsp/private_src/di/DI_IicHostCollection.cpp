018e15b9b21876796be99f1601244c093b1c780e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-08 13:00:41 +0800

    bsp::MutexIicHost _mutex_gpio_iic_host{&_gpio_iic_host};

--- atk-stm32h743-bsp/private_src/di/DI_IicHostCollection.cpp
+++ atk-stm32h743-bsp/private_src/di/DI_IicHostCollection.cpp
@@ -3,6 +3,7 @@
 #include <bsp-interface/di/iic.h>
 #include <bsp-interface/di/interrupt.h>
 #include <bsp-interface/iic/GpioSoftwareIicHost.h>
+#include <bsp-interface/iic/MutexIicHost.h>
 
 namespace
 {
@@ -11,16 +12,17 @@ namespace
     private:
         /// @brief 连接着 EEROM 芯片和 PCF8574T 芯片的 GPIO 模拟 IIC 主机接口。
         bsp::GpioSoftwareIicHost _gpio_iic_host{"gpio_iic_host", "PH4", "PH5"};
+        bsp::MutexIicHost _mutex_gpio_iic_host{&_gpio_iic_host};
 
-        void Add(bsp::IIicHost *host)
+        void Add(bsp::IIicHost &host)
         {
-            _dic.Add(host->Name(), host);
+            _dic.Add(host.Name(), &host);
         }
 
     public:
         Initializer()
         {
-            Add(&_gpio_iic_host);
+            Add(_mutex_gpio_iic_host);
         }
 
         base::Dictionary<std::string, bsp::IIicHost *> _dic;
