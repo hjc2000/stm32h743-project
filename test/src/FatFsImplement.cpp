93be2e39ec6d2461d79ee377d9850dbecebd0939
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-09 20:17:56 +0800

    注释

--- test/src/FatFsImplement.cpp
+++ test/src/FatFsImplement.cpp
@@ -29,7 +29,8 @@ extern "C"
     DSTATUS disk_initialize(BYTE pdrv)
     {
         // 这里假设初始化总是成功的
-        return 0; // 返回0表示成功
+        // 返回0表示成功
+        return 0;
     }
 
     /// @brief 获取磁盘的状态
@@ -38,7 +39,8 @@ extern "C"
     DSTATUS disk_status(BYTE pdrv)
     {
         // 假设磁盘总是处于良好状态
-        return 0; // 返回0表示正常
+        // 返回0表示正常
+        return 0;
     }
 
     /// @brief 从磁盘读取指定数量的扇区到缓冲区
@@ -53,7 +55,7 @@ extern "C"
                   _buffer + (sector + count) * _sector_size,
                   buff);
 
-        return RES_OK; // 成功
+        return RES_OK;
     }
 
     DRESULT disk_write(BYTE pdrv, const BYTE *buff, LBA_t sector, UINT count)
@@ -62,7 +64,7 @@ extern "C"
                   buff + count * _sector_size,
                   _buffer + sector * _sector_size);
 
-        return RES_OK; // 成功
+        return RES_OK;
     }
 
     /// @brief 控制磁盘设备
@@ -74,23 +76,27 @@ extern "C"
     {
         switch (cmd)
         {
-        case CTRL_SYNC: // 刷新缓存到磁盘
+        case CTRL_SYNC:
             {
+                // 刷新缓存到磁盘
                 // 对于模拟磁盘，这里不需要做任何事情
                 break;
             }
-        case GET_SECTOR_COUNT: // 获取磁盘上的总扇区数
+        case GET_SECTOR_COUNT:
             {
+                // 获取磁盘上的总扇区数
                 *reinterpret_cast<DWORD *>(buff) = _sector_count;
                 break;
             }
-        case GET_SECTOR_SIZE: // 获取每个扇区的大小
+        case GET_SECTOR_SIZE:
             {
+                // 获取每个扇区的大小
                 *reinterpret_cast<WORD *>(buff) = _sector_size;
                 break;
             }
-        case GET_BLOCK_SIZE: // 获取擦除块的大小（以扇区为单位）
+        case GET_BLOCK_SIZE:
             {
+                // 获取擦除块的大小（以扇区为单位）
                 // 对于模拟磁盘，我们假设擦除块大小等于一个扇区
                 *reinterpret_cast<DWORD *>(buff) = 1;
                 break;
@@ -101,11 +107,12 @@ extern "C"
             }
         default:
             {
-                return RES_PARERR; // 参数错误
+                // 参数错误
+                return RES_PARERR;
             }
         }
 
-        return RES_OK; // 成功
+        return RES_OK;
     }
 
     /// @brief 使用 std::chrono 获取当前时间并转换为 FatFs 时间戳
