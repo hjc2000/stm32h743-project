9774ea689242563ca6db7fc3d6bf11e36049c6cd
hjc2000 <hjc.666@foxmail.com>
2024-12-13 09:19:09 +0800

    clang-format

--- atk-stm32h743-bsp/private_src/EhternetPort.h
+++ atk-stm32h743-bsp/private_src/EhternetPort.h
@@ -5,48 +5,48 @@
 
 namespace bsp
 {
-    enum class PhyRegister : uint32_t
-    {
-        BCR = 0,
-        BSR = 1,
-        REGISTER2 = 2,
-        REGISTER3 = 3,
-    };
+	enum class PhyRegister : uint32_t
+	{
+		BCR = 0,
+		BSR = 1,
+		REGISTER2 = 2,
+		REGISTER3 = 3,
+	};
 
-    /// @brief 以太网端口。
-    class EhternetPort :
-        public bsp::IEthernetPort
-    {
-    private:
-        bsp::IEthernetController *_controller = &DI_EthernetController();
+	/// @brief 以太网端口。
+	class EhternetPort :
+		public bsp::IEthernetPort
+	{
+	private:
+		bsp::IEthernetController *_controller = &DI_EthernetController();
 
-        void ChipInitialize();
+		void ChipInitialize();
 
-    public:
-        static_function EhternetPort &Instance();
+	public:
+		static_function EhternetPort &Instance();
 
-        /// @brief 以太网控制器的名称。
-        /// @return
-        std::string Name() const override;
+		/// @brief 以太网控制器的名称。
+		/// @return
+		std::string Name() const override;
 
-        /// @brief 打开以太网端口。
-        /// @param mac MAC 地址。
-        void Open(base::Mac const &mac) override;
+		/// @brief 打开以太网端口。
+		/// @param mac MAC 地址。
+		void Open(base::Mac const &mac) override;
 
-        /// @brief 读 PHY 的寄存器
-        /// @param register_index 寄存器索引。
-        /// @return
-        uint32_t ReadPHYRegister(uint32_t register_index) override;
+		/// @brief 读 PHY 的寄存器
+		/// @param register_index 寄存器索引。
+		/// @return
+		uint32_t ReadPHYRegister(uint32_t register_index) override;
 
-        /// @brief 写 PHY 的寄存器。
-        /// @param register_index 寄存器索引。
-        /// @param value
-        void WritePHYRegister(uint32_t register_index, uint32_t value) override;
+		/// @brief 写 PHY 的寄存器。
+		/// @param register_index 寄存器索引。
+		/// @param value
+		void WritePHYRegister(uint32_t register_index, uint32_t value) override;
 
-        /// @brief 复位 PHY 芯片。
-        void ResetPHY() override;
+		/// @brief 复位 PHY 芯片。
+		void ResetPHY() override;
 
-        /// @brief 自动协商。
-        void EnableAutoNegotiation() override;
-    };
+		/// @brief 自动协商。
+		void EnableAutoNegotiation() override;
+	};
 } // namespace bsp
