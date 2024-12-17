787c7e35c6372b0fa6e92252b45edf2c75031b5e
hjc2000 <hjc.666@foxmail.com>
2024-12-17 10:30:03 +0800

    删除重复的pnal.h

--- atk-stm32h743-bsp/src/p-net/src/common/pf_file.c
+++ atk-stm32h743-bsp/src/p-net/src/common/pf_file.c
@@ -24,13 +24,13 @@
 
 #ifdef UNIT_TEST
 #define pnal_clear_file mock_pnal_clear_file
-#define pnal_load_file  mock_pnal_load_file
-#define pnal_save_file  mock_pnal_save_file
+#define pnal_load_file mock_pnal_load_file
+#define pnal_save_file mock_pnal_save_file
 #endif
 
-#include "pf_includes.h"
 #include "pf_block_reader.h"
 #include "pf_block_writer.h"
+#include "pf_includes.h"
 
 #include <inttypes.h>
 #include <stdlib.h>
@@ -44,13 +44,13 @@
 /* The configurable constant PNET_MAX_FILENAME_SIZE should be at least
  * as large as the longest filename used, including termination.
  */
-//CC_STATIC_ASSERT (PNET_MAX_FILENAME_SIZE >= sizeof (PF_FILENAME_IP));
-//CC_STATIC_ASSERT (PNET_MAX_FILENAME_SIZE >= sizeof (PF_FILENAME_IM));
-//CC_STATIC_ASSERT (PNET_MAX_FILENAME_SIZE >= sizeof (PF_FILENAME_DIAGNOSTICS));
-//CC_STATIC_ASSERT (PNET_MAX_FILENAME_SIZE >= sizeof (PF_FILENAME_PDPORT_1));
-//CC_STATIC_ASSERT (PNET_MAX_FILENAME_SIZE >= sizeof (PF_FILENAME_PDPORT_2));
-//CC_STATIC_ASSERT (PNET_MAX_FILENAME_SIZE >= sizeof (PF_FILENAME_PDPORT_3));
-//CC_STATIC_ASSERT (PNET_MAX_FILENAME_SIZE >= sizeof (PF_FILENAME_PDPORT_4));
+// CC_STATIC_ASSERT (PNET_MAX_FILENAME_SIZE >= sizeof (PF_FILENAME_IP));
+// CC_STATIC_ASSERT (PNET_MAX_FILENAME_SIZE >= sizeof (PF_FILENAME_IM));
+// CC_STATIC_ASSERT (PNET_MAX_FILENAME_SIZE >= sizeof (PF_FILENAME_DIAGNOSTICS));
+// CC_STATIC_ASSERT (PNET_MAX_FILENAME_SIZE >= sizeof (PF_FILENAME_PDPORT_1));
+// CC_STATIC_ASSERT (PNET_MAX_FILENAME_SIZE >= sizeof (PF_FILENAME_PDPORT_2));
+// CC_STATIC_ASSERT (PNET_MAX_FILENAME_SIZE >= sizeof (PF_FILENAME_PDPORT_3));
+// CC_STATIC_ASSERT (PNET_MAX_FILENAME_SIZE >= sizeof (PF_FILENAME_PDPORT_4));
 
 /**
  * @internal
@@ -67,257 +67,259 @@
  * @return  0  if the operation succeeded.
  *          -1 if not found or an error occurred.
  */
-int pf_file_join_directory_filename (
-   const char * directory,
-   const char * filename,
-   char * fullpath,
-   size_t size)
+int pf_file_join_directory_filename(
+	char const *directory,
+	char const *filename,
+	char *fullpath,
+	size_t size)
 {
-   bool use_directory = true;
+	bool use_directory = true;
 
-   if (filename == NULL || fullpath == NULL)
-   {
-      LOG_ERROR (
-         PNET_LOG,
-         "FILE(%d): Filename and outputbuffer must be given!\n",
-         __LINE__);
-      return -1;
-   }
-   if (strlen (filename) < 1)
-   {
-      LOG_ERROR (PNET_LOG, "FILE(%d): Too short filename!\n", __LINE__);
-      return -1;
-   }
+	if (filename == NULL || fullpath == NULL)
+	{
+		LOG_ERROR(
+			PNET_LOG,
+			"FILE(%d): Filename and outputbuffer must be given!\n",
+			__LINE__);
+		return -1;
+	}
+	if (strlen(filename) < 1)
+	{
+		LOG_ERROR(PNET_LOG, "FILE(%d): Too short filename!\n", __LINE__);
+		return -1;
+	}
 
-   if (directory == NULL)
-   {
-      use_directory = false;
-   }
-   else
-   {
-      if (strlen (directory) == 0)
-      {
-         use_directory = false;
-      }
-   }
+	if (directory == NULL)
+	{
+		use_directory = false;
+	}
+	else
+	{
+		if (strlen(directory) == 0)
+		{
+			use_directory = false;
+		}
+	}
 
-   if (use_directory == true)
-   {
-      if (strlen (directory) + strlen (filename) + 2 > size)
-      {
-         LOG_ERROR (
-            PNET_LOG,
-            "FILE(%d): Too long directory and filename! Max total length %zu\n",
-            __LINE__,
-            size);
-         return -1;
-      }
+	if (use_directory == true)
+	{
+		if (strlen(directory) + strlen(filename) + 2 > size)
+		{
+			LOG_ERROR(
+				PNET_LOG,
+				"FILE(%d): Too long directory and filename! Max total length %zu\n",
+				__LINE__,
+				size);
+			return -1;
+		}
 
-      strcpy (fullpath, directory);
-      if (fullpath[strlen (directory) - 1] != '/')
-      {
-         strcat (fullpath, "/");
-      }
-      strcat (fullpath, filename);
-   }
-   else
-   {
-      if (strlen (filename) + 1 > size)
-      {
-         LOG_ERROR (
-            PNET_LOG,
-            "FILE(%d): Too long filename! Max total length %zu\n",
-            __LINE__,
-            size);
-         return -1;
-      }
-      strcpy (fullpath, filename);
-   }
+		strcpy(fullpath, directory);
+		if (fullpath[strlen(directory) - 1] != '/')
+		{
+			strcat(fullpath, "/");
+		}
+		strcat(fullpath, filename);
+	}
+	else
+	{
+		if (strlen(filename) + 1 > size)
+		{
+			LOG_ERROR(
+				PNET_LOG,
+				"FILE(%d): Too long filename! Max total length %zu\n",
+				__LINE__,
+				size);
+			return -1;
+		}
+		strcpy(fullpath, filename);
+	}
 
-   return 0;
+	return 0;
 }
 
-int pf_file_load (
-   const char * directory,
-   const char * filename,
-   void * p_object,
-   size_t size)
+int pf_file_load(
+	char const *directory,
+	char const *filename,
+	void *p_object,
+	size_t size)
 {
-   uint8_t versioning_buffer[8] = {0}; /* Two uint32_t */
-   pf_get_info_t bufferinfo;
-   uint32_t version = 0;
-   uint32_t magic = 0;
-   uint16_t pos = 0;
-   char path[PNET_MAX_FILE_FULLPATH_SIZE];
-   uint32_t start_time_us = 0;
+	uint8_t versioning_buffer[8] = {0}; /* Two uint32_t */
+	pf_get_info_t bufferinfo;
+	uint32_t version = 0;
+	uint32_t magic = 0;
+	uint16_t pos = 0;
+	char path[PNET_MAX_FILE_FULLPATH_SIZE];
+	uint32_t start_time_us = 0;
 
-   bufferinfo.p_buf = versioning_buffer;
-   bufferinfo.len = sizeof (versioning_buffer);
-   bufferinfo.is_big_endian = true;
-   bufferinfo.result = PF_PARSE_OK;
+	bufferinfo.p_buf = versioning_buffer;
+	bufferinfo.len = sizeof(versioning_buffer);
+	bufferinfo.is_big_endian = true;
+	bufferinfo.result = PF_PARSE_OK;
 
-   if (
-      pf_file_join_directory_filename (
-         directory,
-         filename,
-         path,
-         PNET_MAX_FILE_FULLPATH_SIZE) != 0)
-   {
-      return -1;
-   }
+	if (
+		pf_file_join_directory_filename(
+			directory,
+			filename,
+			path,
+			PNET_MAX_FILE_FULLPATH_SIZE) != 0)
+	{
+		return -1;
+	}
 
-   /* Read file */
-   start_time_us = os_get_current_time_us();
-   if (
-      pnal_load_file (
-         path,
-         versioning_buffer,
-         sizeof (versioning_buffer),
-         p_object,
-         size) != 0)
-   {
-      return -1;
-   }
-   LOG_DEBUG (
-      PNET_LOG,
-      "FILE(%d): Did read file %s Access time %" PRIu32 " ms.\n",
-      __LINE__,
-      path,
-      ((os_get_current_time_us() - start_time_us) / 1000));
+	/* Read file */
+	start_time_us = os_get_current_time_us();
 
-   magic = pf_get_uint32 (&bufferinfo, &pos);
-   version = pf_get_uint32 (&bufferinfo, &pos);
+	if (
+		pnal_load_file(
+			path,
+			versioning_buffer,
+			sizeof(versioning_buffer),
+			p_object,
+			size) != 0)
+	{
+		return -1;
+	}
 
-   /* Check file magic bytes */
-   if (magic != PF_FILE_MAGIC)
-   {
-      LOG_ERROR (
-         PNET_LOG,
-         "FILE(%d): Wrong file magic bytes in file %s\n",
-         __LINE__,
-         path);
-      return -1;
-   }
+	LOG_DEBUG(
+		PNET_LOG,
+		"FILE(%d): Did read file %s Access time %" PRIu32 " ms.\n",
+		__LINE__,
+		path,
+		((os_get_current_time_us() - start_time_us) / 1000));
 
-   /* Check file version */
-   if (version != PF_FILE_VERSION)
-   {
-      LOG_WARNING (
-         PNET_LOG,
-         "FILE(%d): Wrong file version identifier in file %s  Expected %" PRIu32
-         " but got %" PRIu32 ".\n",
-         __LINE__,
-         path,
-         (uint32_t)PF_FILE_VERSION,
-         version);
-      return -1;
-   }
+	magic = pf_get_uint32(&bufferinfo, &pos);
+	version = pf_get_uint32(&bufferinfo, &pos);
 
-   return 0;
+	/* Check file magic bytes */
+	if (magic != PF_FILE_MAGIC)
+	{
+		LOG_ERROR(
+			PNET_LOG,
+			"FILE(%d): Wrong file magic bytes in file %s\n",
+			__LINE__,
+			path);
+		return -1;
+	}
+
+	/* Check file version */
+	if (version != PF_FILE_VERSION)
+	{
+		LOG_WARNING(
+			PNET_LOG,
+			"FILE(%d): Wrong file version identifier in file %s  Expected %" PRIu32
+			" but got %" PRIu32 ".\n",
+			__LINE__,
+			path,
+			(uint32_t)PF_FILE_VERSION,
+			version);
+		return -1;
+	}
+
+	return 0;
 }
 
-int pf_file_save (
-   const char * directory,
-   const char * filename,
-   const void * p_object,
-   size_t size)
+int pf_file_save(
+	char const *directory,
+	char const *filename,
+	void const *p_object,
+	size_t size)
 {
-   char path[PNET_MAX_FILE_FULLPATH_SIZE]; /**< Terminated string */
-   uint8_t versioning_buffer[8] = {0};     /**< Two uint32_t */
-   uint16_t pos = 0;
-   int ret = 0;
-   uint32_t start_time_us = 0;
+	char path[PNET_MAX_FILE_FULLPATH_SIZE]; /**< Terminated string */
+	uint8_t versioning_buffer[8] = {0};     /**< Two uint32_t */
+	uint16_t pos = 0;
+	int ret = 0;
+	uint32_t start_time_us = 0;
 
-   if (
-      pf_file_join_directory_filename (
-         directory,
-         filename,
-         path,
-         PNET_MAX_FILE_FULLPATH_SIZE) != 0)
-   {
-      return -1;
-   }
+	if (
+		pf_file_join_directory_filename(
+			directory,
+			filename,
+			path,
+			PNET_MAX_FILE_FULLPATH_SIZE) != 0)
+	{
+		return -1;
+	}
 
-   pf_put_uint32 (
-      true,
-      PF_FILE_MAGIC,
-      sizeof (versioning_buffer),
-      versioning_buffer,
-      &pos); /* Big endian */
-   pf_put_uint32 (
-      true,
-      PF_FILE_VERSION,
-      sizeof (versioning_buffer),
-      versioning_buffer,
-      &pos); /* Big endian */
+	pf_put_uint32(
+		true,
+		PF_FILE_MAGIC,
+		sizeof(versioning_buffer),
+		versioning_buffer,
+		&pos); /* Big endian */
+	pf_put_uint32(
+		true,
+		PF_FILE_VERSION,
+		sizeof(versioning_buffer),
+		versioning_buffer,
+		&pos); /* Big endian */
 
-   start_time_us = os_get_current_time_us();
-   ret = pnal_save_file (
-      path,
-      versioning_buffer,
-      sizeof (versioning_buffer),
-      p_object,
-      size);
-   LOG_DEBUG (
-      PNET_LOG,
-      "FILE(%d): Did save file %s Access time %" PRIu32 " ms.\n",
-      __LINE__,
-      path,
-      ((os_get_current_time_us() - start_time_us) / 1000));
+	start_time_us = os_get_current_time_us();
+	ret = pnal_save_file(
+		path,
+		versioning_buffer,
+		sizeof(versioning_buffer),
+		p_object,
+		size);
+	LOG_DEBUG(
+		PNET_LOG,
+		"FILE(%d): Did save file %s Access time %" PRIu32 " ms.\n",
+		__LINE__,
+		path,
+		((os_get_current_time_us() - start_time_us) / 1000));
 
-   return ret;
+	return ret;
 }
 
-int pf_file_save_if_modified (
-   const char * directory,
-   const char * filename,
-   const void * p_object,
-   void * p_tempobject,
-   size_t size)
+int pf_file_save_if_modified(
+	char const *directory,
+	char const *filename,
+	void const *p_object,
+	void *p_tempobject,
+	size_t size)
 {
-   bool save = false;
-   int ret = 0; /* Assume no changes */
+	bool save = false;
+	int ret = 0; /* Assume no changes */
 
-   memset (p_tempobject, 0, size);
+	memset(p_tempobject, 0, size);
 
-   if (pf_file_load (directory, filename, p_tempobject, size) == 0)
-   {
-      if (memcmp (p_tempobject, p_object, size) != 0)
-      {
-         ret = 1;
-         save = true;
-      }
-   }
-   else
-   {
-      ret = 2;
-      save = true;
-   }
+	if (pf_file_load(directory, filename, p_tempobject, size) == 0)
+	{
+		if (memcmp(p_tempobject, p_object, size) != 0)
+		{
+			ret = 1;
+			save = true;
+		}
+	}
+	else
+	{
+		ret = 2;
+		save = true;
+	}
 
-   if (save == true)
-   {
-      if (pf_file_save (directory, filename, p_object, size) != 0)
-      {
-         ret = -1;
-      }
-   }
+	if (save == true)
+	{
+		if (pf_file_save(directory, filename, p_object, size) != 0)
+		{
+			ret = -1;
+		}
+	}
 
-   return ret;
+	return ret;
 }
 
-void pf_file_clear (const char * directory, const char * filename)
+void pf_file_clear(char const *directory, char const *filename)
 {
-   char path[PNET_MAX_FILE_FULLPATH_SIZE];
+	char path[PNET_MAX_FILE_FULLPATH_SIZE];
 
-   if (
-      pf_file_join_directory_filename (
-         directory,
-         filename,
-         path,
-         PNET_MAX_FILE_FULLPATH_SIZE) != 0)
-   {
-      return;
-   }
+	if (
+		pf_file_join_directory_filename(
+			directory,
+			filename,
+			path,
+			PNET_MAX_FILE_FULLPATH_SIZE) != 0)
+	{
+		return;
+	}
 
-   return pnal_clear_file (path);
+	return pnal_clear_file(path);
 }
