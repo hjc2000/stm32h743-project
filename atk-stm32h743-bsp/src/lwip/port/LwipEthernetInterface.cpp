5889b2a117a13e5a01deaeb27953d00a18dc2b13
hjc2000 <hjc.666@foxmail.com>
2024-12-18 13:29:18 +0800

    initialization_callback

--- atk-stm32h743-bsp/src/lwip/port/LwipEthernetInterface.cpp
+++ atk-stm32h743-bsp/src/lwip/port/LwipEthernetInterface.cpp
@@ -438,27 +438,25 @@ void bsp::LwipEthernetInterface::Open()
 #endif
 
 	// 添加网卡
+	auto initialization_callback = [](netif *p) -> err_t
 	{
-		auto callback = [](netif *p) -> err_t
-		{
-			bsp::LwipEthernetInterface::Instance().InitializingNetifCallbackFunc();
-			return err_enum_t::ERR_OK;
-		};
+		bsp::LwipEthernetInterface::Instance().InitializingNetifCallbackFunc();
+		return err_enum_t::ERR_OK;
+	};
 
-		/* 向网卡列表中添加一个网口 */
-		netif *netif_add_result = netif_add(&_lwip_netif,
-											reinterpret_cast<ip_addr_t const *>(&_ip_address),
-											reinterpret_cast<ip_addr_t const *>(&_netmask),
-											reinterpret_cast<ip_addr_t const *>(&_gateway),
-											nullptr,
-											callback,
-											tcpip_input);
+	/* 向网卡列表中添加一个网口 */
+	netif *netif_add_result = netif_add(&_lwip_netif,
+										reinterpret_cast<ip_addr_t const *>(&_ip_address),
+										reinterpret_cast<ip_addr_t const *>(&_netmask),
+										reinterpret_cast<ip_addr_t const *>(&_gateway),
+										nullptr,
+										initialization_callback,
+										tcpip_input);
 
-		if (netif_add_result == nullptr)
-		{
-			DI_Console().WriteLine("添加网卡失败。");
-			throw std::runtime_error{"添加网卡失败。"};
-		}
+	if (netif_add_result == nullptr)
+	{
+		DI_Console().WriteLine("添加网卡失败。");
+		throw std::runtime_error{"添加网卡失败。"};
 	}
 
 	// 添加网卡成功后将其设置为默认网卡
