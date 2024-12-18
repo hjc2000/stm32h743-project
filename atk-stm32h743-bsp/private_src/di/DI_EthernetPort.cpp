cd8d6d17780d2a357dd05bb0792bcd7fddab04b6
hjc2000 <hjc.666@foxmail.com>
2024-12-18 16:20:40 +0800

    将EthernetController独立成库

--- atk-stm32h743-bsp/private_src/di/DI_EthernetPort.cpp
+++ atk-stm32h743-bsp/private_src/di/DI_EthernetPort.cpp
@@ -1,13 +1,5 @@
 #include <AtkLAN8720A_EhternetPort.h>
 #include <bsp-interface/di/ethernet.h>
-#include <EthernetController.h>
-
-/// @brief 以太网控制器。
-/// @return
-bsp::IEthernetController &DI_EthernetController()
-{
-	return bsp::EthernetController::Instance();
-}
 
 /// @brief 以太网端口。
 /// @return
