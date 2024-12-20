3bba3df2b6c011abb30818bb118bc303e2182cc4
hjc2000 <hjc.666@foxmail.com>
2024-12-20 11:08:51 +0800

    MutexEthernetPort

--- atk-stm32h743-bsp/private_src/di/DI_EthernetPort.cpp
+++ atk-stm32h743-bsp/private_src/di/DI_EthernetPort.cpp
@@ -1,11 +1,43 @@
 #include <AtkApolloV1EthernetPort.h>
 #include <AtkApolloV2EthernetPort.h>
+#include <base/di/SingletonGetter.h>
 #include <bsp-interface/di/ethernet.h>
+#include <bsp-interface/di/interrupt.h>
+#include <bsp-interface/ethernet/MutexEthernetPort.h>
+
+namespace
+{
+	class Getter :
+		public base::SingletonGetter<bsp::MutexEthernetPort>
+	{
+	public:
+		std::unique_ptr<bsp::MutexEthernetPort> Create() override
+		{
+			return std::unique_ptr<bsp::MutexEthernetPort>{
+				new bsp::MutexEthernetPort{
+					&bsp::AtkApolloV1EthernetPort::Instance(),
+					// &bsp::AtkApolloV2EthernetPort::Instance(),
+				},
+			};
+		}
+
+		void Lock() override
+		{
+			DI_DisableGlobalInterrupt();
+		}
+
+		void Unlock() override
+		{
+			DI_EnableGlobalInterrupt();
+		}
+	};
+
+} // namespace
 
 /// @brief 以太网端口。
 /// @return
 bsp::IEthernetPort &DI_EthernetPort()
 {
-	return bsp::AtkApolloV1EthernetPort::Instance();
-	// return bsp::AtkApolloV2EthernetPort::Instance();
+	Getter g;
+	return g.Instance();
 }
