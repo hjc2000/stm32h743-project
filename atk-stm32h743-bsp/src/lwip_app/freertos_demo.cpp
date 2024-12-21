e07a1ca32688166cf07216b06ae176edf42c4960
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-21 22:34:06 +0800

    freertos_demo

--- atk-stm32h743-bsp/src/lwip_app/freertos_demo.cpp
+++ atk-stm32h743-bsp/src/lwip_app/freertos_demo.cpp
@@ -21,9 +21,6 @@
 /* 接收数据缓冲区 */
 uint8_t g_lwip_demo_recvbuf[200];
 
-sockaddr_in local_info; /* 定义Socket地址信息结构体 */
-socklen_t sock_fd;      /* 定义一个Socket接口 */
-
 void freertos_demo()
 {
 	lwip::NetifWrapper _netif_wrapper;
@@ -42,8 +39,19 @@ void freertos_demo()
 		}
 	}
 
+	socklen_t sock_fd{};                      /* 定义一个Socket接口 */
+	sock_fd = socket(AF_INET, SOCK_DGRAM, 0); /* 建立一个新的socket连接 */
+
+	sockaddr_in local_info{}; /* 定义Socket地址信息结构体 */
+	local_info.sin_len = sizeof(local_info);
+	local_info.sin_family = AF_INET;                /* IPv4地址 */
+	local_info.sin_port = htons(8080);              /* 设置端口号 */
+	local_info.sin_addr.s_addr = htons(INADDR_ANY); /* 设置本地IP地址 */
+
+	bind(sock_fd, (sockaddr *)&local_info, sizeof(sockaddr_in));
+
 	DI_TaskManager().Create(
-		[]()
+		[&]()
 		{
 			local_info.sin_addr.s_addr = inet_addr("192.168.1.203"); /* 需要发送的远程IP地址 */
 			std::string sending_message{"UDP testing message"};
@@ -62,16 +70,6 @@ void freertos_demo()
 		},
 		512);
 
-	memset(&local_info, 0, sizeof(struct sockaddr_in)); /* 将服务器地址清空 */
-	local_info.sin_len = sizeof(local_info);
-	local_info.sin_family = AF_INET;                /* IPv4地址 */
-	local_info.sin_port = htons(8080);              /* 设置端口号 */
-	local_info.sin_addr.s_addr = htons(INADDR_ANY); /* 设置本地IP地址 */
-
-	sock_fd = socket(AF_INET, SOCK_DGRAM, 0); /* 建立一个新的socket连接 */
-
-	/* 建立绑定 */
-	bind(sock_fd, (struct sockaddr *)&local_info, sizeof(struct sockaddr_in));
 	while (true)
 	{
 		memset(g_lwip_demo_recvbuf, 0, sizeof(g_lwip_demo_recvbuf));
