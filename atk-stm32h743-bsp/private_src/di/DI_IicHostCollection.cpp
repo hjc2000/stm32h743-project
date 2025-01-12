f1794cddeb42e49311d14cfb8e1b2b2d1dc293a5
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-01-12 23:37:54 +0800

    TaskSingletonGetter

--- atk-stm32h743-bsp/private_src/di/DI_IicHostCollection.cpp
+++ atk-stm32h743-bsp/private_src/di/DI_IicHostCollection.cpp
@@ -1,56 +1,56 @@
 #include <base/container/Dictionary.h>
-#include <base/di/SingletonGetter.h>
 #include <bsp-interface/di/iic.h>
 #include <bsp-interface/di/interrupt.h>
 #include <bsp-interface/iic/GpioSoftwareIicHost.h>
 #include <bsp-interface/iic/MutexIicHost.h>
+#include <bsp-interface/TaskSingletonGetter.h>
 
 namespace
 {
-    class Initializer
-    {
-    private:
-        /// @brief 连接着 EEROM 芯片和 PCF8574T 芯片的 GPIO 模拟 IIC 主机接口。
-        bsp::GpioSoftwareIicHost _gpio_iic_host{"gpio_iic_host", "PH4", "PH5"};
-        bsp::MutexIicHost _mutex_gpio_iic_host{&_gpio_iic_host};
+	class Initializer
+	{
+	private:
+		/// @brief 连接着 EEROM 芯片和 PCF8574T 芯片的 GPIO 模拟 IIC 主机接口。
+		bsp::GpioSoftwareIicHost _gpio_iic_host{"gpio_iic_host", "PH4", "PH5"};
+		bsp::MutexIicHost _mutex_gpio_iic_host{&_gpio_iic_host};
 
-        void Add(bsp::IIicHost &host)
-        {
-            _dic.Add(host.Name(), &host);
-        }
+		void Add(bsp::IIicHost &host)
+		{
+			_dic.Add(host.Name(), &host);
+		}
 
-    public:
-        Initializer()
-        {
-            Add(_mutex_gpio_iic_host);
-        }
+	public:
+		Initializer()
+		{
+			Add(_mutex_gpio_iic_host);
+		}
 
-        base::Dictionary<std::string, bsp::IIicHost *> _dic;
-    };
+		base::Dictionary<std::string, bsp::IIicHost *> _dic;
+	};
 
-    class Getter :
-        public base::SingletonGetter<Initializer>
-    {
-    public:
-        std::unique_ptr<Initializer> Create() override
-        {
-            return std::unique_ptr<Initializer>{new Initializer{}};
-        }
+	class Getter :
+		public base::SingletonGetter<Initializer>
+	{
+	public:
+		std::unique_ptr<Initializer> Create() override
+		{
+			return std::unique_ptr<Initializer>{new Initializer{}};
+		}
 
-        void Lock() override
-        {
-            DI_DisableGlobalInterrupt();
-        }
+		void Lock() override
+		{
+			DI_DisableGlobalInterrupt();
+		}
 
-        void Unlock() override
-        {
-            DI_EnableGlobalInterrupt();
-        }
-    };
+		void Unlock() override
+		{
+			DI_EnableGlobalInterrupt();
+		}
+	};
 } // namespace
 
 base::IDictionary<std::string, bsp::IIicHost *> const &DI_IicHostCollection()
 {
-    Getter o;
-    return o.Instance()._dic;
+	Getter o;
+	return o.Instance()._dic;
 }
