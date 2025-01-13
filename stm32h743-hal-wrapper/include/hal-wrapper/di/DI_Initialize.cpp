775d52e70b1dbeb6900611476654660265e846fc
hjc2000 <hjc.666@foxmail.com>
2025-01-13 11:49:57 +0800

    注释

--- stm32h743-hal-wrapper/include/hal-wrapper/di/DI_Initialize.cpp
+++ stm32h743-hal-wrapper/include/hal-wrapper/di/DI_Initialize.cpp
@@ -13,6 +13,14 @@ namespace
 
 		{
 			base::Dictionary<std::string, int> factors;
+
+			/**
+			 * HSE 接的是 25MHz 的晶振。
+			 * 		@li m 是对 PLL 输入时钟的分频。25MHz 的晶振分频后是 5MHz.
+			 * 		@li n 是对经过 m 分频后的信号进行倍频。5MHz 被 192 倍频后是 960MHz.
+			 * 		@li p,q,r 是对 n 的输出进行分频。这里是 2 分频，于是得到的都是 480MHz 的时钟信号。
+			 * 			其中 p 的输出作为系统时钟的输入。于是系统时钟频率是 480MHz. 系统时钟直接输入给 CPU.
+			 */
 			factors.Set("m", 5);
 			factors.Set("n", 192);
 			factors.Set("p", 2);
