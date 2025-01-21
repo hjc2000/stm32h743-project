fd946a92dbc72658af9e86e5270ceaa81953a1a6
hjc2000 <hjc.666@foxmail.com>
2025-01-21 15:18:37 +0800

    project_name

--- test/stflash-release-download.ps1
+++ test/stflash-release-download.ps1
@@ -16,13 +16,13 @@ try
 
 	if ($LASTEXITCODE)
 	{
-		throw "$source_path 配置失败"
+		throw "$project_name 配置失败"
 	}
 
 	ninja -j12
 	if ($LASTEXITCODE)
 	{
-		throw "$source_path 编译失败"
+		throw "$project_name 编译失败"
 	}
 
 	ninja install
