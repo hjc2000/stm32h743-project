c83b64b547616b1e7974e54a54b3a29b43710569
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-03-11 20:19:38 +0800

    objcopy_cmd

--- test/stflash-release-download.ps1
+++ test/stflash-release-download.ps1
@@ -54,9 +54,14 @@ try
 		}
 	}
 
-	arm-none-eabi-objcopy -O binary `
-		"$install_path/bin/${project_name}.elf" `
+	$objcopy_cmd = @(
+		"$env:cpp_lib_build_scripts_path/.toolchain/arm-none-eabi-14.2/bin/arm-none-eabi-objcopy"
+		"-O binary"
+		"$install_path/bin/${project_name}.elf"
 		"$install_path/bin/${project_name}.bin"
+	)
+	$objcopy_cmd = $objcopy_cmd -join " "
+	Invoke-Expression $objcopy_cmd
 
 	st-flash --reset write "${install_path}/bin/${project_name}.bin" 0x8000000
 	if ($LASTEXITCODE)
