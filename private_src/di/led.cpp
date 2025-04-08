df8a29d7c72a577a86e7bec9865bd5df429d5512
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-08 22:08:31 +0800

    PREINIT

--- private_src/di/led.cpp
+++ private_src/di/led.cpp
@@ -1,15 +1,15 @@
+#include "base/define.h"
 #include "base/peripheral/IDigitalLed.h"
 #include <base/container/Dictionary.h>
 #include <bsp-interface/di/interrupt.h>
-#include <bsp-interface/TaskSingletonGetter.h>
 #include <DigitalLed.h>
 
 namespace
 {
-	class Initializer
+	class DictionaryProvider
 	{
 	public:
-		Initializer()
+		DictionaryProvider()
 		{
 			_dic.Add("red_led", &bsp::RedDigitalLed::Instance());
 			_dic.Add("green_led", &bsp::GreenDigitalLed::Instance());
@@ -18,24 +18,14 @@ namespace
 		base::Dictionary<std::string, base::led::IDigitalLed *> _dic;
 	};
 
-	/// @brief 单例获取器
-	class Getter :
-		public bsp::TaskSingletonGetter<Initializer>
-	{
-	public:
-		std::unique_ptr<Initializer> Create() override
-		{
-			return std::unique_ptr<Initializer>{new Initializer{}};
-		}
-	};
 } // namespace
 
-/// @brief 数字 LED 灯集合。
-/// @return
+PREINIT(base::led::DigitalLedCollection)
+
 base::IDictionary<std::string, base::led::IDigitalLed *> const &base::led::DigitalLedCollection()
 {
-	Getter g;
-	return g.Instance()._dic;
+	static DictionaryProvider o;
+	return o._dic;
 }
 
 base::led::IDigitalLed &base::led::RedDigitalLed()
