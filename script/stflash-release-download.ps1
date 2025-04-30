bf77d1e115a7bbe1d50310732725d27840f201d9
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-30 22:53:09 +0800

    . "script/prepare.ps1"

--- script/stflash-release-download.ps1
+++ script/stflash-release-download.ps1
@@ -1,44 +1,4 @@
-$ErrorActionPreference = "Stop"
-
-# region 项目参数
-
-$project_name = "stm32h743-project"
-$cmake_config = "arm-none-eabi-cortex-m7-release"
-$platform = "arm-none-eabi-cortex-m7"
-$workspace_path = Get-Location
-$build_path = "$workspace_path/jc_build"
-$install_path = "$env:cpp_lib_build_scripts_path/${platform}/.libs/${project_name}"
-
-# endregion
-
-# region 如果还没编译安装，就先编译安装。
-
-New-Item -Path $build_path -ItemType Directory -Force
-Push-Location $build_path
-try
-{
-	cmake -G "Ninja" $workspace_path `
-		--preset "$cmake_config"
-
-	if ($LASTEXITCODE)
-	{
-		throw "配置失败"
-	}
-
-	ninja -j12
-	if ($LASTEXITCODE)
-	{
-		throw "编译失败"
-	}
-
-	ninja install
-}
-finally
-{
-	Pop-Location
-}
-
-# endregion
+. "script/prepare.ps1"
 
 Push-Location $install_path
 try
