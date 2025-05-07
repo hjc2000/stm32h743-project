d840cb0e0ba160e1c8c854571853fe146c8d0033
hjc2000 <hjc.666@foxmail.com>
2025-05-07 08:56:55 +0800

    Key2

--- private_src/key/Key2.cpp
+++ private_src/key/Key2.cpp
@@ -1,42 +1,13 @@
 #include "Key2.h"
 #include "base/embedded/gpio/gpio_parameter.h"
-#include "base/embedded/systick/systick.h"
-#include "base/unit/Nanoseconds.h"
-#include <chrono>
 
 bsp::Key2::Key2()
 {
 	_pin.InitializeAsInputMode(base::gpio::PullMode::PullUp,
-							   base::gpio::TriggerEdge::FallingEdge);
-
-	_pin.RegisterInterruptCallback([this]()
-								   {
-									   _pressed = true;
-								   });
+							   base::gpio::TriggerEdge::Disable);
 }
 
 bool bsp::Key2::KeyIsDown()
 {
-	if (_pressed)
-	{
-		_last_check_time = base::systick::system_time_stamp();
-		_pressed = false;
-		_checking = true;
-	}
-
-	if (_checking)
-	{
-		base::Nanoseconds current_time = base::systick::system_time_stamp();
-		if (current_time - _last_check_time > base::Nanoseconds{std::chrono::milliseconds{20}})
-		{
-			return !_pin.ReadPin();
-			_checking = false;
-		}
-		else
-		{
-			return true;
-		}
-	}
-
-	return false;
+	return !_pin.ReadPin();
 }
--- private_src/key/Key2.h
+++ private_src/key/Key2.h
@@ -1,22 +1,20 @@
 #pragma once
 #include "base/embedded/gpio/gpio_parameter.h"
 #include "base/embedded/gpio/GpioPin.h"
-#include "base/unit/Nanoseconds.h"
 #include "base/UsageStateManager.h"
 #include "key_handle.h"
 
 namespace bsp
 {
+	///
 	/// @brief 位于 PC13 引脚。另一端接地，所以需要上拉使用。
+	///
 	class Key2 :
 		public base::key::key_handle
 	{
 	private:
 		base::UsageStateManager<Key2> _usage_state_manager{};
 		base::gpio::GpioPin _pin{base::gpio::PortEnum::PortC, 13};
-		bool volatile _pressed = false;
-		bool volatile _checking = false;
-		base::Nanoseconds _last_check_time;
 
 	public:
 		Key2();
