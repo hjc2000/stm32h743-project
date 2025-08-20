3bfba546e684aaa4a49942ab331af55b37929061
hjc2000 <hjc.666@foxmail.com>
2025-08-20 13:46:19 +0800

    重构SDRAM堆初始化代码，添加FMC GPIO初始化注释

--- private_src/initialize.cpp
+++ private_src/initialize.cpp
@@ -125,53 +125,56 @@ void bsp::initialize_clock()
 
 void bsp::initialize_sdram_heap()
 {
-	std::vector<base::gpio::GpioPin> pins{
-		base::gpio::GpioPin{base::gpio::PortEnum::PortC, 0},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortC, 2},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortC, 3},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortD, 0},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortD, 1},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortD, 8},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortD, 9},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortD, 10},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortD, 14},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortD, 15},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortE, 0},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortE, 1},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortE, 7},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortE, 8},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortE, 9},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortE, 10},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortE, 11},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortE, 12},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortE, 13},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortE, 14},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortE, 15},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortF, 0},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortF, 1},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortF, 2},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortF, 3},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortF, 4},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortF, 5},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortF, 11},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortF, 12},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortF, 13},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortF, 14},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortF, 15},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortG, 0},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortG, 1},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortG, 2},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortG, 4},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortG, 5},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortG, 8},
-		base::gpio::GpioPin{base::gpio::PortEnum::PortG, 15},
-	};
-
-	for (auto &pin : pins)
+	// 初始化 FMC 的 GPIO
 	{
-		pin.InitializeAsAlternateFunctionMode(12,
-											  base::gpio::PullMode::PullUp,
-											  base::gpio::DriveMode::PushPull);
+		std::vector<base::gpio::GpioPin> pins{
+			base::gpio::GpioPin{base::gpio::PortEnum::PortC, 0},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortC, 2},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortC, 3},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortD, 0},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortD, 1},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortD, 8},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortD, 9},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortD, 10},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortD, 14},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortD, 15},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortE, 0},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortE, 1},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortE, 7},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortE, 8},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortE, 9},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortE, 10},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortE, 11},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortE, 12},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortE, 13},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortE, 14},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortE, 15},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortF, 0},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortF, 1},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortF, 2},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortF, 3},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortF, 4},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortF, 5},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortF, 11},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortF, 12},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortF, 13},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortF, 14},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortF, 15},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortG, 0},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortG, 1},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortG, 2},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortG, 4},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortG, 5},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortG, 8},
+			base::gpio::GpioPin{base::gpio::PortEnum::PortG, 15},
+		};
+
+		for (auto &pin : pins)
+		{
+			pin.InitializeAsAlternateFunctionMode(12,
+												  base::gpio::PullMode::PullUp,
+												  base::gpio::DriveMode::PushPull);
+		}
 	}
 
 	base::sdram::SdramController sdram_controller{1};
