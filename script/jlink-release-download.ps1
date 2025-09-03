8dca33ee233bc89c02ba47ee497d31a3b4d5a14d
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-09-03 23:34:04 +0800

    调试时gdb自己会对比和下载固件，不需要我再下载

--- script/jlink-release-download.ps1
+++ script/jlink-release-download.ps1
@@ -3,28 +3,13 @@
 Push-Location $install_path
 try
 {
-	if (Test-Path -Path "$install_path/bin/${project_name}.bin")
-	{
-		# bin 文件存在
-		$is_newer = is-file-newer-than-reference.ps1 `
-			-TargetFilePath "$install_path/bin/${project_name}.elf" `
-			-ReferenceFilePath "$install_path/bin/${project_name}.bin"
-
-		if (-not $is_newer)
-		{
-			# bin 文件存在，则只有 elf 比 bin 更新时才需要重新生成 bin 并下载。
-			# 所以直接返回。
-			Write-Host "不需要下载。"
-			return
-		}
-	}
-
 	$objcopy_cmd = @(
 		"$env:cpp_lib_build_scripts_path/.toolchain/arm-none-eabi-14.2/bin/arm-none-eabi-objcopy"
 		"-O binary"
 		"$install_path/bin/${project_name}.elf"
 		"$install_path/bin/${project_name}.bin"
 	)
+
 	$objcopy_cmd = $objcopy_cmd -join " "
 	Invoke-Expression $objcopy_cmd
 
