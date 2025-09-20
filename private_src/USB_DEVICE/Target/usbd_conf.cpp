f23689da12062eba0177c39ae86251566b8d4975
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-09-21 00:37:03 +0800

    refactor(USB): 重构USB设备配置文件为C++并添加类型转换

--- private_src/USB_DEVICE/Target/usbd_conf.cpp
+++ private_src/USB_DEVICE/Target/usbd_conf.cpp
@@ -21,6 +21,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "stm32h7xx.h"
 #include "stm32h7xx_hal.h"
+#include "stm32h7xx_hal_pcd.h"
 #include "usbd_cdc.h"
 #include "usbd_core.h"
 #include "usbd_def.h"
@@ -400,7 +401,7 @@ USBD_StatusTypeDef USBD_LL_DeInit(USBD_HandleTypeDef *pdev)
 	HAL_StatusTypeDef hal_status = HAL_OK;
 	USBD_StatusTypeDef usb_status = USBD_OK;
 
-	hal_status = HAL_PCD_DeInit(pdev->pData);
+	hal_status = HAL_PCD_DeInit(reinterpret_cast<PCD_HandleTypeDef *>(pdev->pData));
 
 	usb_status = USBD_Get_USB_Status(hal_status);
 
@@ -417,7 +418,7 @@ USBD_StatusTypeDef USBD_LL_Start(USBD_HandleTypeDef *pdev)
 	HAL_StatusTypeDef hal_status = HAL_OK;
 	USBD_StatusTypeDef usb_status = USBD_OK;
 
-	hal_status = HAL_PCD_Start(pdev->pData);
+	hal_status = HAL_PCD_Start(reinterpret_cast<PCD_HandleTypeDef *>(pdev->pData));
 
 	usb_status = USBD_Get_USB_Status(hal_status);
 
@@ -434,7 +435,7 @@ USBD_StatusTypeDef USBD_LL_Stop(USBD_HandleTypeDef *pdev)
 	HAL_StatusTypeDef hal_status = HAL_OK;
 	USBD_StatusTypeDef usb_status = USBD_OK;
 
-	hal_status = HAL_PCD_Stop(pdev->pData);
+	hal_status = HAL_PCD_Stop(reinterpret_cast<PCD_HandleTypeDef *>(pdev->pData));
 
 	usb_status = USBD_Get_USB_Status(hal_status);
 
@@ -454,7 +455,7 @@ USBD_StatusTypeDef USBD_LL_OpenEP(USBD_HandleTypeDef *pdev, uint8_t ep_addr, uin
 	HAL_StatusTypeDef hal_status = HAL_OK;
 	USBD_StatusTypeDef usb_status = USBD_OK;
 
-	hal_status = HAL_PCD_EP_Open(pdev->pData, ep_addr, ep_mps, ep_type);
+	hal_status = HAL_PCD_EP_Open(reinterpret_cast<PCD_HandleTypeDef *>(pdev->pData), ep_addr, ep_mps, ep_type);
 
 	usb_status = USBD_Get_USB_Status(hal_status);
 
@@ -472,7 +473,7 @@ USBD_StatusTypeDef USBD_LL_CloseEP(USBD_HandleTypeDef *pdev, uint8_t ep_addr)
 	HAL_StatusTypeDef hal_status = HAL_OK;
 	USBD_StatusTypeDef usb_status = USBD_OK;
 
-	hal_status = HAL_PCD_EP_Close(pdev->pData, ep_addr);
+	hal_status = HAL_PCD_EP_Close(reinterpret_cast<PCD_HandleTypeDef *>(pdev->pData), ep_addr);
 
 	usb_status = USBD_Get_USB_Status(hal_status);
 
@@ -490,7 +491,7 @@ USBD_StatusTypeDef USBD_LL_FlushEP(USBD_HandleTypeDef *pdev, uint8_t ep_addr)
 	HAL_StatusTypeDef hal_status = HAL_OK;
 	USBD_StatusTypeDef usb_status = USBD_OK;
 
-	hal_status = HAL_PCD_EP_Flush(pdev->pData, ep_addr);
+	hal_status = HAL_PCD_EP_Flush(reinterpret_cast<PCD_HandleTypeDef *>(pdev->pData), ep_addr);
 
 	usb_status = USBD_Get_USB_Status(hal_status);
 
@@ -508,7 +509,7 @@ USBD_StatusTypeDef USBD_LL_StallEP(USBD_HandleTypeDef *pdev, uint8_t ep_addr)
 	HAL_StatusTypeDef hal_status = HAL_OK;
 	USBD_StatusTypeDef usb_status = USBD_OK;
 
-	hal_status = HAL_PCD_EP_SetStall(pdev->pData, ep_addr);
+	hal_status = HAL_PCD_EP_SetStall(reinterpret_cast<PCD_HandleTypeDef *>(pdev->pData), ep_addr);
 
 	usb_status = USBD_Get_USB_Status(hal_status);
 
@@ -526,7 +527,7 @@ USBD_StatusTypeDef USBD_LL_ClearStallEP(USBD_HandleTypeDef *pdev, uint8_t ep_add
 	HAL_StatusTypeDef hal_status = HAL_OK;
 	USBD_StatusTypeDef usb_status = USBD_OK;
 
-	hal_status = HAL_PCD_EP_ClrStall(pdev->pData, ep_addr);
+	hal_status = HAL_PCD_EP_ClrStall(reinterpret_cast<PCD_HandleTypeDef *>(pdev->pData), ep_addr);
 
 	usb_status = USBD_Get_USB_Status(hal_status);
 
@@ -564,7 +565,7 @@ USBD_StatusTypeDef USBD_LL_SetUSBAddress(USBD_HandleTypeDef *pdev, uint8_t dev_a
 	HAL_StatusTypeDef hal_status = HAL_OK;
 	USBD_StatusTypeDef usb_status = USBD_OK;
 
-	hal_status = HAL_PCD_SetAddress(pdev->pData, dev_addr);
+	hal_status = HAL_PCD_SetAddress(reinterpret_cast<PCD_HandleTypeDef *>(pdev->pData), dev_addr);
 
 	usb_status = USBD_Get_USB_Status(hal_status);
 
@@ -584,7 +585,7 @@ USBD_StatusTypeDef USBD_LL_Transmit(USBD_HandleTypeDef *pdev, uint8_t ep_addr, u
 	HAL_StatusTypeDef hal_status = HAL_OK;
 	USBD_StatusTypeDef usb_status = USBD_OK;
 
-	hal_status = HAL_PCD_EP_Transmit(pdev->pData, ep_addr, pbuf, size);
+	hal_status = HAL_PCD_EP_Transmit(reinterpret_cast<PCD_HandleTypeDef *>(pdev->pData), ep_addr, pbuf, size);
 
 	usb_status = USBD_Get_USB_Status(hal_status);
 
@@ -604,7 +605,7 @@ USBD_StatusTypeDef USBD_LL_PrepareReceive(USBD_HandleTypeDef *pdev, uint8_t ep_a
 	HAL_StatusTypeDef hal_status = HAL_OK;
 	USBD_StatusTypeDef usb_status = USBD_OK;
 
-	hal_status = HAL_PCD_EP_Receive(pdev->pData, ep_addr, pbuf, size);
+	hal_status = HAL_PCD_EP_Receive(reinterpret_cast<PCD_HandleTypeDef *>(pdev->pData), ep_addr, pbuf, size);
 
 	usb_status = USBD_Get_USB_Status(hal_status);
 
